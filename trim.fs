90be5b93f389959cb146132eb51a6b2a5a92ed83
programandala.net <>
2013-10-25 19:04:00 +0000

    Update from development backup 201310252104.
    FossilOrigin-Name: 4fc04462a7b0159b1576bde2c9c00206142b2811652eb0b966cb91e24ea33564

---
+++ trim.fs
@@ -0,0 +1,15 @@
+\ galope/trim.fs/
+
+\ This file is part of Galope
+
+\ Copyright (C) 2013 Marcos Cruz (programandala.net)
+
+\ History
+\ 2013-10-22 Added.
+
+require ./minus-leading.fs
+
+: trim ( ca1 len1 -- ca2 len2 )
+  -leading -trailing
+  ;
+
