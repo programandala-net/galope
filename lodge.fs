bdd669b3151f0e75876310eccb85d5d01c2f46e9
programandala.net <>
2014-02-21 15:18:00 +0000

    Update from development backup 201402211618.
    FossilOrigin-Name: 6bc7204bd35260d8de012924638ea60b666014611e1e6db8e1a7d5bc0ee000f1

---
+++ lodge.fs
@@ -0,0 +1,174 @@
+\ galope/lodge.fs
+
+\ This file is part of Galope
+
+\ Copyright (C) 2014 Marcos Cruz (programandala.net)
+
+\ --------------------------------------------------------------
+\ About
+
+\ This code provides words that create variables and values whose data
+\ is stored into an automatically growing buffer. The variables and
+\ values store in their own body an offset to the actual data in the
+\ buffer.  This makes it possible to save the whole buffer to a binary
+\ file and restore it later, e.g.  for game sessions.
+
+\ The first name of this code was "zbuffer"; then it was renamed to
+\ "pack", but unfortunately the name was already taken in Galope, by a
+\ simple one-liner; then it was renamed to "stow", just to discover
+\ (in The Collaborative International Dictionary of English v.0.48
+\ [gcide]) that "stow" is only a transitive verb, not a substantive,
+\ and some words didn't fit well; finally "lodge" was chosen...
+
+\ --------------------------------------------------------------
+\ History
+
+\ 2014-02-16 Written.
+\ 2014-02-17 New: 'lodge-value', 'lodge-to' and related words.
+\ 2014-02-19 New: 'lodge-address-value'.
+\ 2014-02-20 Change: "index" and "relative pointer" corrected to "offset"
+\   in the comments; additional comment in 'lodge-address-value';
+\   '(cell-lodge-value:)' factored out from 'lodge-value' and
+\   'lodge-address-value'; '>lodge' renamed to 'lodge+'.
+\ 2014-02-20 New: 'lodge-save-mem'.
+\ 2014-02-21 Fix: '[lodge-to]' and '[lodge-2to]' were wrong.
+\   '[lodge-to]', before the fix:
+\     ' postpone xt>lodge postpone literal postpone !
+\     \ first bug: 'xt>lodge' should not be postponed in this case.
+\     \ second bug: the absolute lodge address must not be compiled,
+\     \   because it may change.
+\   After the fix:
+\     ' postpone literal postpone xt>lodge postpone !
+\   Now the xt is compiled and the lodge address is calculated at run-time.
+
+\ --------------------------------------------------------------
+\ Buffer
+
+variable lodge  \ buffer address
+variable /lodge  \ length in address units
+0 /lodge !  \ [only for compatibility; Gforth doesn't need it]
+
+0 allocate throw lodge !  \ an empty buffer at first
+
+: lodge+  ( +n -- a )
+  \ Current absolute address of a lodge offset. 
+  lodge @ +
+  ;
+: lodge-adjust  ( u a -- +n )
+  \ u = additional address units already allocated in the lodge
+  \ a = new address of the lodge
+  \ +n = offset to the new free space
+  \      (it's the same than the previous length of the lodge)
+  lodge !  /lodge @  swap /lodge +!
+  ;
+: lodge-resize  ( u -- a wior )
+  \ u = new size of the lodge
+  \ a = new address of the lodge
+  lodge @ swap resize
+  ;
+: lodge-allocate  ( u -- +n wior )
+  \ u = additional address units required in the lodge
+  \ +n = lodge offset to the additional free space
+  dup /lodge @ + lodge-resize >r lodge-adjust r>
+  ;
+: (lodge-erase)  ( u +n -- )
+  \ Erase u address units from a lodge offset.
+  lodge+ swap erase
+  ;
+: lodge-erase  ( +n u -- )
+  \ Erase u address units from a lodge offset.
+  swap (lodge-erase)
+  ;
+
+\ --------------------------------------------------------------
+\ Variables
+
+: lodge-variable-does>  ( -- a )
+  \ Behaviour of a lodge variable. 
+  does> ( -- a ) ( dfa ) @ lodge+
+  ;
+: (lodge-variable)  ( u -- )
+  \ Compile a lodge variable of u address units.
+  dup lodge-allocate throw dup , (lodge-erase)
+  ;
+: lodge-variable  ( "name" -- )
+  \ Create a lodge variable and init it with zero.
+  create cell (lodge-variable) lodge-variable-does>
+  ;
+: lodge-2variable  ( "name" -- )
+  \ Create a lodge double variable and init it with zero.
+  create 2 cells (lodge-variable) lodge-variable-does>
+  ;
+
+\ --------------------------------------------------------------
+\ Values
+
+: (lodge-value)  ( u -- a )
+  \ Compile a lodge value of u address units.
+  lodge-allocate throw dup , lodge+
+  ;
+: (cell-lodge-value:)  ( x "name" -- )
+  \ Create a 1-cell lodge value.
+  create  cell (lodge-value) !
+  ;
+: lodge-value  ( n "name" -- )
+  \ Create a lodge value.
+  (cell-lodge-value:)  does> ( -- n ) ( dfa ) @ lodge+ @
+  ;
+: lodge-address-value  ( +n "name" -- )
+  \ Create a lodge address value.
+  \ +n = lodge offset 
+  \ This kind of lodge-value stores a lodge offset but returns the
+  \ corresponding absolute address.  This is useful for creating
+  \ lodge-values that point to data space in the lodge but have to be
+  \ used as ordinary Forth values.
+  (cell-lodge-value:)  does> ( -- a ) ( dfa ) @ lodge+ @ lodge+
+  ;
+: lodge-2value  ( d "name" -- )
+  \ Create a lodge value.
+  create 2 cells (lodge-value) 2!
+  does> ( -- d ) ( dfa ) @ lodge+ 2@
+  ;
+
+\ --------------------------------------------------------------
+\ To
+
+: xt>lodge  ( xt -- a )
+  \ Absolute lodge address from the xt of a lodge-variable or a
+  \ lodge-value.
+  >body @ lodge+  
+  ;
+
+: <lodge-to>  ( x "name" -- )
+  \ Change the content of a lodge-variable or a lodge-value.
+  ' xt>lodge !
+  ; 
+: [lodge-to]  ( compilation: x "name" -- )
+  \ Change the content of a lodge-variable or a lodge-value.
+  ' postpone literal postpone xt>lodge postpone !
+  ; immediate 
+' <lodge-to>
+' [lodge-to]
+interpret/compile: lodge-to
+
+: <lodge-2to>  ( x "name" -- )
+  \ Change the content of a lodge-2variable or a lodge-2value.
+  ' xt>lodge 2!
+  ; 
+: [lodge-2to]  ( compilation: d "name" -- )
+  \ Change the content of a lodge-2variable or a lodge-2value.
+  ' xt>lodge postpone 2literal postpone 2!
+  ; immediate 
+' <lodge-2to>
+' [lodge-2to]
+interpret/compile: lodge-2to
+
+\ --------------------------------------------------------------
+\ Misc
+
+: lodge-save-mem  ( a1 len -- +n len )
+  \ Copy a memory block into the lodge.
+  \ Written after Gforth's 'save-mem'.
+  swap >r  dup lodge-allocate throw  swap over lodge+ over  r> -rot move
+  ;
+
