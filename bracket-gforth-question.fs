d38a44a67cf73237ecf5eba8498d0535ba609c72
programandala.net <>
2016-06-20 12:44:00 +0000

    Update from development backup 201606201444.
    FossilOrigin-Name: 7787b03034bbd0aa31ff3d7f290e644a68c3a5c876eac1aeb41414ab149bc346

--- bracket-gforth-question.fs
+++ bracket-gforth-question.fs
@@ -1,13 +1,23 @@
 \ galope/bracket-gforth-question.fs
+\ [gforth?]
 
 \ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
 
-\ Copyright (C) 2012 Marcos Cruz (programandala.net)
+\ Author: Marcos Cruz (programandala.net), 2012, 2016
+
+\ Licence
+\
+\ You may do whatever you want with this work, so long as you
+\ retain the copyright/authorship/acknowledgment/credit
+\ notice(s) and this license in all redistributed copies and
+\ derived works.  There is no warranty.
 
 \ History
-\ 2012-05-08 Added.
+\
+\ 2012-05-08: Added.
+\ 2016-01-16: Updated header.
 
 s" gforth" environment?
 dup [if]  nip nip  [then]
 constant [gforth?] immediate
-
--- home.fs
+++ home.fs
@@ -1,19 +1,27 @@
 \ galope/home.fs
+\ home
 
 \ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
 
-\ Copyright (C) 2012 Marcos Cruz (programandala.net)
+\ Author: Marcos Cruz (programandala.net), 2012,2016
 
-\ This file is public domain.
+\ Licence
+\
+\ You may do whatever you want with this work, so long as you
+\ retain the copyright/authorship/acknowledgment/credit
+\ notice(s) and this license in all redistributed copies and
+\ derived works.  There is no warranty.
 
 \ History
+\
 \ 2012-05-06: First version.
 \ 2012-09-14: Code reformated.
+\ 2016-01-16: Updated header and layout.
 
 [undefined] home [if]
   : home  ( -- )
-    0 dup at-xy
-    ;
+    0 dup at-xy  ;
 [then]
 
 
