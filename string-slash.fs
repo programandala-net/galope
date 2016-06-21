c7a65da3f14d09149f229bc168e5710daee93d39
programandala.net <>
2016-06-21 09:53:37 +0000

    Update to resume development.
    FossilOrigin-Name: a640b37a39b2f1803d7e2c873d7539519629afac7b5ea823503ad83054733d0e

--- string-slash.fs
+++ string-slash.fs
@@ -7,12 +7,13 @@
 
 \ History
 \ 2012-12-10: Added.
-\ 2014-11-02: Change: stack comments. 
+\ 2014-11-02: Change: stack comments.
+\ 2016-04-03: Improved comment.
 
+\ Credit
 \ Taken from Charscan by Wil Baden (2002-2003).
 
 : string/  ( ca1 len1 len2 -- ca2 len2 )
-  \ Return the len2 ending characters of a string.
-  >r + r@ - r>
-  ;
+  >r + r@ - r>  ;
+  \ Return the _len2_ ending characters of string _ca1 len1_.
 
