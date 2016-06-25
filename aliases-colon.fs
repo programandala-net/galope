cee71f6a3455f105eef7c732206711f6bfb66337
Marcos Cruz <mercurial-repo@programandala.net>
2016-06-25 16:33:27 +0000

    Add `aliases:` and `immediate-aliases:`.
    FossilOrigin-Name: 6393e06514d9cae7339f94beea5f3ede31886eccbbfcdc32cdb1e6c01581d386

---
+++ aliases-colon.fs
@@ -0,0 +1,38 @@
+\ aliases-colon.fs
+\ `aliases:`
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ By Marcos Cruz (programandala.net)
+
+\ 2016-06-25: Extract from the project _Asalto y castigo_ and rename.
+
+require ./colon-alias.fs
+require ./module.fs
+
+module: galope-aliases-colon-module
+
+: parse-alias  ( -- ca len )
+  begin   parse-name dup 0=
+  while   2drop refill 0=
+          abort" Error: `;aliases` missing"
+  repeat  ;
+  \ Parse and return the next alias of the list started by
+  \ `aliases{`.
+
+: another-alias?  ( -- ca len f )
+  parse-alias 2dup s" ;aliases" compare  ;
+  \ Parse and return the next alias _ca len_ of the list started by
+  \ `aliases{`. If _f_ is false, _ca len_ is the word "}aliases",
+  \ which marks the end of the list.
+
+export
+
+: aliases:  (  xt "name#0" ... "name#n" ";aliases" -- )
+  begin  dup another-alias? ( xt xt ca len wf )
+  while  :alias
+  repeat  2drop 2drop  ;
+  \ Create any number of aliases of _xt_, until ";aliases" is parsed.
+
+;module
