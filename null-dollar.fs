c21d3163b24b2a4ccc8fc76a6a73c1b0edf33cfb
programandala.net <>
2016-07-11 16:15:56 +0000

    Add `null$`.
    FossilOrigin-Name: 6c4c181c3e57e1f3773fb52e8ec206f3baab80a0fc89fec1d507e25881128bab

---
+++ null-dollar.fs
@@ -0,0 +1,16 @@
+\ galope/null-dollar.fs
+\ `null$`
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2016.
+
+\ ==============================================================
+
+pad 0 2constant null$
+
+\ ==============================================================
+
+\ 2016-07-11: Move from "Asalto y castigo"
+\ (http://programandala.net/en.program.asalto_y_castigo.forth.html).
