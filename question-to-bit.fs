42a8a5376749845c4c93191b4094fe9723283dd8
programandala.net <>
2016-07-19 11:16:58 +0000

    Add `>bit` and `?>bit`.
    FossilOrigin-Name: 8d0e877703b18f60814a7310c59f573c126958216aae18080d0edee28a485201

---
+++ question-to-bit.fs
@@ -0,0 +1,21 @@
+\ galope/question-to-bit.fs
+\ `?>bit`
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2016.
+
+\ ==============================================================
+
+require ./to-bit.fs
+
+: ?>bit  ( n -- x )  >bit dup 0= -11 and throw  ;
+  \ Convert bit number _n_ to its bit mask _x_;
+  \ throw error "Result out of range" if result is zero.
+
+\ ==============================================================
+\ History
+
+\ 2016-07-14: Write.
+\ 2016-07-19: Fix typo. Update stack notation.
