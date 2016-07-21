6aec31e14287c08fd0adb98ac20ee20b23aa4865
Marcos Cruz <mercurial-repo@programandala.net>
2016-07-21 19:46:13 +0000

    Add text concatenation for FFL's dynamic strings. Two words not provided by Forth Foundation Library's "str" module: concatenate strings with a space separator. Useful to concatenate texts. That's why the "txt" suffix is used.
    FossilOrigin-Name: 22b3f7ec45e19e7ae4ec8e1fd4dba1ed0abff317e7a390a27d197f9758ab4e6c

---
+++ str-prepend-txt.fs
@@ -0,0 +1,28 @@
+\ galope/str-prepend-txt.fs
+\ `str-prepend-txt`
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2016.
+
+\ ==============================================================
+
+require ffl/str.fs
+  \ Forth Foundation Library (http://irdvo.github.io/ffl/)
+
+require ./str-txt-concatenation-question.fs  \ `str-txt-concatenation?`
+
+\ ==============================================================
+
+: str-prepend-txt  ( ca len str -- )
+  2dup str-txt-concatenation?
+  if  bl over str-prepend-char  then  str-prepend-string  ;
+  \ Prepend a text _ca len_ to a FFL's dynamic string _str_,
+  \ with a space separator if needed.
+
+\ ==============================================================
+\ History
+
+\ 2016-07-21: Extract from _Asalto y castigo_
+\ (http://programandala.net/es.programa.asalto_y_castigo.forth.html).
