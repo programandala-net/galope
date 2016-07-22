1aac75aa3b25b993d684d6b0ed4e71f12df3519b
programandala.net <>
2016-07-22 12:20:41 +0000

    Add `choose{` and `2choose{`. `2choose{` is the logical name of the old `s{`, while `choose{` is its logical counterpart. The stack used by the old `s{` has been factored out to its own module. `s{` has been kept as an optional alias, for convenience, because it's used a lot in some projects, to choose a random string from a set, and it's shorter, what makes code lighter.
    FossilOrigin-Name: 6a9f6798347b826742b1b2af87683d9ea36a07a2a20bfc44277480f09a508ee5

---
+++ choose-stack.fs
@@ -0,0 +1,67 @@
+\ galope/choose-stack.fs
+\ A stack used by `choose{` and `2choose`.
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2011, 2012, 2013, 2014,
+\ 2016.
+
+\ ==============================================================
+
+require ./module.fs
+require ./two-choose.fs
+require ./plus-plus.fs
+require ./minus-minus.fs
+
+\ ==============================================================
+
+8 constant /choose-stack
+  \ Maximum size of the choose stack in cells. This is the maximum
+  \ number of nestings for `choose{ }choose` and `2choose{ }2choose`.
+
+variable choose-stack-used
+  \ Cells used in the choose stack.
+
+create choose-stack /choose-stack cells allot
+
+: init-choose-stack  ( -- )  0 choose-stack-used !  ;
+
+init-choose-stack
+
+: choose-stack-tos ( -- a )
+  choose-stack-used @ 1- cells choose-stack +  ;
+  \ Address of the top of choose stack.
+
+: choose-stack-full? ( -- f )
+  choose-stack-used @ /choose-stack =  ;
+  \ Is the choose stack full?
+
+: choose-stack-empty? ( -- f )
+  choose-stack-used @ 0=  ;
+  \ Is the choose stack empty?
+
+: ?choose-stack-full  ( -- )
+  choose-stack-full? abort" The choose stack is full."  ;
+  \ Abort if the choose stack is full.
+
+: >choose-stack ( n -- )
+  ?choose-stack-full
+  choose-stack-used ++ choose-stack-tos !  ;
+  \ Store _n_ in the choose stack.
+
+: ?choose-stack-empty  ( -- )
+  choose-stack-empty? abort" The choose stack is empty."  ;
+  \ Abort if the choose stack is empty.
+
+: <choose-stack ( -- n )
+  ?choose-stack-empty
+  choose-stack-tos @ choose-stack-used --  ;
+  \ Fetch the top of the choose stack.
+
+\ ==============================================================
+\ History
+
+\ 2016-07-22: Move from the old module <random_strings.fs>, improve
+\ and rename the words, in order to reuse them for the new `2choose{
+\ }2choose` and `choose{ }choose`.
---
+++ two-choose-curly-bracket.fs
@@ -0,0 +1,37 @@
+\ galope/two-choose-curly-bracket.fs
+\ `2choose{`
+\ Random selection for a compiled set of double cells.
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2011, 2012, 2013, 2014,
+\ 2016.
+
+\ Description: Tools to compile several strings and return one of them
+\ by chance at run-time.
+
+\ ==============================================================
+
+require random.fs  \ Gforth's `random`
+
+require ./choose-stack.fs
+require ./two-choose.fs
+
+\ ==============================================================
+\ Operators
+
+: 2choose{ ( -- )
+  depth >choose-stack  ;
+  \ Start a set of double numbers (or strings) to choose from.
+
+: }2choose ( d1 ... dn -- d' )
+  depth <choose-stack - 2 / 2choose  ;
+  \ Chose a random _d'_ from the set _d1 ... dn_
+  \ stacked since the last unresolved `2choose{`.
+
+\ ==============================================================
+\ History
+
+\ 2016-07-22: Copy, improve and rename `s{ }s` from the old module
+\ <random_strings.fs> 
