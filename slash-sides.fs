3582f83ff8bcf86c4ee773cec323db8f5c7fb015
programandala.net <>
2016-07-29 17:27:22 +0000

    Add `/sentences`.
    FossilOrigin-Name: 5114bbeff1f23ca7c43e0aa5ef32c8f6f387f53f09da714d65ec22869db2c3a9

--- slash-sides.fs
+++ slash-sides.fs
@@ -2,32 +2,34 @@
 \ /sides
 
 \ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
 
-\ Copyright (C) 2013 Marcos Cruz (programandala.net)
+\ Author: Marcos Cruz (programandala.net), 2013.
 
-\ 2013-07-11 Added. Taken from Fendo
-\ (http://programandala.net/en.program.fendo)
-\ 2013-11-06 Changed the stack notation of flag.
+\ Credit: This word was inspired by Wil Baden's `hunt` (Charscan
+\ library, 2003-02-17, public domain).
+
+\ ==============================================================
 
 require ./sides.fs  \ 'sides'
 
-: /sides  { ca1 len1 ca2 len2 -- ca1 len1' ca3 len3 wf }
-  \ Search a string ca1 len1
-  \ for the first occurence of a substring ca2 len2.
-  \ Divide the string ca1 len1 in two parts: return both sides
-  \ of the substring ca2 len2 (first occurence), excluding the
-  \ substring ca2 len2 itself.
-  \ 2013-06-07
-  \ This word was inspired by Wil Baden's 'hunt'
-  \ (Charscan library, 2003-02-17, public domain).
-  \ ca1 len1  = string
-  \ ca2 len2  = substring
-  \ ca1 len1' = left side (or whole string if not found)
-  \ ca3 len3  = right side (or empty string if not found)
-  \ wf = found?
-  \ Note: ca3 len3 can be empty also when wf is true.
+: /sides  { ca1 len1 ca2 len2 -- ca1 len1' ca3 len3 f }
   ca1 len1 ca2 len2 search dup >r
   if    ca1 len1 len2 sides
   else  over 0  \ fake right side
-  then  r>
-  ;
+  then  r>  ;
+  \ Search a string _ca1 len1_ for the first occurence of a substring
+  \ _ca2 len2_ and return the substring _ca1 len1'_ at its left and
+  \ _ca3 len3_ at its right.  If _ca2 len2_ is found, _f_ is true.
+  \ Any returned substring can be empty, even if _ca2 len2_ is found,
+  \ depending on its position in _ca1 len1_.
+
+\ ==============================================================
+\ History
+
+\ 2013-07-11: Added. Taken from Fendo
+\ (http://programandala.net/en.program.fendo.html)
+\
+\ 2013-11-06: Changed the stack notation of flag.
+\
+\ 2016-07-29: Update source layout and file header.
