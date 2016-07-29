48042e82330f8faae080bfe2d2f52c4dd34d2f06
Marcos Cruz <mercurial-repo@programandala.net>
2016-07-29 17:27:22 +0000

    Add `/sentences`.
    FossilOrigin-Name: 5114bbeff1f23ca7c43e0aa5ef32c8f6f387f53f09da714d65ec22869db2c3a9

---
+++ slash-sentences.fs
@@ -0,0 +1,24 @@
+\ galope/slash-sentences.fs
+\ `/sentences`
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2016.
+
+\ ==============================================================
+
+require ./slash-sides.fs  \ '/sides'
+
+: /sentences  ( ca len -- ca#1 len#1 ... ca#n len#n n )
+  depth >r
+  begin  s" . " /sides 0=  until  2drop
+  depth r> 2 - - 2/  ;
+  \ Split a string _ca len_ into sentences (which were separated by a
+  \ period and a space) _ca#1 len#1 ... ca#n len#n_, returning the
+  \ number of resulting strings _n_.
+
+\ ==============================================================
+\ History
+
+\ 2016-07-29: Write, after the code of `/csv`.
