3b567bce6afe12ee5dad5f52eb3a243f2203bb1b
Marcos Cruz <mercurial-repo@programandala.net>
2017-06-30 13:29:28 +0000

    Improve documentation.
    FossilOrigin-Name: 20ea2778e022829c3afb23ea1206d54aa6223a573b070a4c3097744b753e0463

--- slash-name.fs
+++ slash-name.fs
@@ -1,20 +1,22 @@
 \ galope/slash-name.fs
 
 \ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
 
-\ Copyright (C) 2015 Marcos Cruz (programandala.net)
+\ Copyright (C) 2015,2017 Marcos Cruz (programandala.net)
 
 \ History
 \
 \ 2015-10-16: Added to the library, taken from fsb2
 \ (http://programandala.net/en.program.fsb2.html) and Solo Forth
 \ (http://programandala.net/en.program.solo_forth.html).
+\
+\ 2017-02-14: Improve documentation.
 
 : /name  ( ca1 len1 -- ca2 len2 ca3 len3 )
-  \ Divide a string at its first name (a substring separated by
-  \ spaces).
-  \ ca1 len1 = Text.
-  \ ca2 len2 = Same text, from the start of its first name.
-  \ ca3 len3 = Same text, from the char after its first name.
   bl skip 2dup bl scan  ;
+  \ Divide a string _ca1 len1_ at its first name (a substring
+  \ separated by spaces), returning substring _ca2 len2_ (from the
+  \ start of the first name in _ca1 len1_) and substring _ca3 len3_
+  \ (from the character after the first name in _ca1 len1_).
 
