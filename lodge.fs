6d1a2a54c97e00d68c8683a13a65498d12978574
programandala.net <>
2017-07-03 11:49:46 +0000

    Update source style and doc of `lodge` module.
    FossilOrigin-Name: 3c0169487b4bcb89e1bec2a61c625a62cde5e860640d149998a99ec37dffecf8

--- lodge.fs
+++ lodge.fs
@@ -2,11 +2,14 @@
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
+\
+\ Last modified 201707031346.
+\ See change log at the end of the file.
 
-\ Copyright (C) 2014 Marcos Cruz (programandala.net)
+\ Copyright (C) 2014,2017 Marcos Cruz (programandala.net)
 
-\ --------------------------------------------------------------
-\ About
+\ ==============================================================
+\ Description
 
 \ This code provides words that create variables, values or any kind
 \ of data whose actual data are stored into a self-growing buffer. The
@@ -29,168 +32,185 @@
 \ [gcide]) that "stow" is only a transitive verb, not a substantive,
 \ and some words didn't fit well; finally "lodge" was chosen...
 
-\ --------------------------------------------------------------
-\ History
-
-\ 2014-02-16 Written for Finto
-\   (<http://programandala.net/en.program.finto.html>).
-\ 2014-02-17 New: 'lodge-value', 'lodge-to' and related words.
-\ 2014-02-19 New: 'lodge-address-value'.
-\ 2014-02-20 Change: "index" and "relative pointer" corrected to "offset"
-\   in the comments; additional comment in 'lodge-address-value';
-\   '(cell-lodge-value:)' factored out from 'lodge-value' and
-\   'lodge-address-value'; '>lodge' renamed to 'lodge+'.
-\ 2014-02-20 New: 'lodge-save-mem'.
-\ 2014-02-21 Fix: '[lodge-to]' and '[lodge-2to]' were wrong.
-\   '[lodge-to]', before the fix:
-\     ' postpone xt>lodge postpone literal postpone !
-\     \ first bug: 'xt>lodge' should not be postponed in this case.
-\     \ second bug: the absolute lodge address must not be compiled,
-\     \   because it may change.
-\   After the fix:
-\     ' postpone literal postpone xt>lodge postpone !
-\   Now the xt is compiled and the lodge address is calculated at run-time.
-\ 2014-02-21: Forked to <galope/lodge-colon.fs>.
-\ 2014-02-21: New: 'lodge-,' and 'lodge-2,'.
-\ 2014-02-21: Fix: 'lodge-resize' now can be used directly by the
-\   application. Solved by moving the trailing code of
-\   'lodge-allocate' to 'lodge-resize'.
-
-\ --------------------------------------------------------------
+\ ==============================================================
 \ Buffer
 
-variable lodge  \ buffer address
-variable /lodge  \ length in address units
-0 /lodge !  \ [only for compatibility; Gforth doesn't need it]
+variable lodge
+  \ Buffer address.
 
-0 allocate throw lodge !  \ an empty buffer at first
+variable /lodge  0 /lodge !
+  \ Length in address units.
 
-: lodge+  ( +n -- a )
-  \ Current absolute address of a lodge offset.
-  lodge @ +
-  ;
-: lodge-update  ( u a -- +n )
+0 allocate throw lodge ! \ an empty buffer at first
+
+: lodge+ ( +n -- a )
+  lodge @ + ;
+  \ Return current absolute address _a_ of a lodge offset _+n_.
+
+: lodge-update ( u a -- +n )
+  lodge !  /lodge @  swap /lodge +! ;
   \ u = additional address units already allocated in the lodge
   \ a = new address of the lodge
   \ +n = offset to the new free space
   \      (it's the same than the previous length of the lodge)
-  lodge !  /lodge @  swap /lodge +!
-  ;
-: lodge-resize  ( u -- +n wior )
+
+: lodge-resize ( u -- +n wior )
+  lodge @ swap resize >r lodge-update r> ;
   \ u = new size of the lodge
   \ +n = lodge offset to the additional free space
-  lodge @ swap resize >r lodge-update r>
-  ;
-: lodge-allocate  ( u -- +n wior )
+
+: lodge-allocate ( u -- +n wior )
+  dup /lodge @ + lodge-resize ;
   \ u = additional address units required in the lodge
   \ +n = lodge offset to the additional free space
-  dup /lodge @ + lodge-resize
-  ;
-: (lodge-erase)  ( u +n -- )
-  \ Erase u address units from a lodge offset.
-  lodge+ swap erase
-  ;
-: lodge-erase  ( +n u -- )
-  \ Erase u address units from a lodge offset.
-  swap (lodge-erase)
-  ;
 
-\ --------------------------------------------------------------
+: (lodge-erase) ( u +n -- )
+  lodge+ swap erase ;
+  \ Erase _u_ address units from a lodge offset.
+
+: lodge-erase ( +n u -- )
+  swap (lodge-erase) ;
+  \ Erase _u_ address units from a lodge offset.
+
+\ ==============================================================
 \ Variables
 
-: lodge-variable-does>  ( -- a )
+: lodge-variable-does> ( -- a )
+  does> ( -- a ) ( dfa ) @ lodge+ ;
   \ Behaviour of a lodge variable.
-  does> ( -- a ) ( dfa ) @ lodge+
-  ;
-: (lodge-variable)  ( u -- )
-  \ Compile a lodge variable of u address units.
-  dup lodge-allocate throw dup , (lodge-erase)
-  ;
-: lodge-variable  ( "name" -- )
-  \ Create a lodge variable and init it with zero.
-  create cell (lodge-variable) lodge-variable-does>
-  ;
-: lodge-2variable  ( "name" -- )
-  \ Create a lodge double variable and init it with zero.
-  create 2 cells (lodge-variable) lodge-variable-does>
-  ;
 
-\ --------------------------------------------------------------
+: (lodge-variable) ( u -- )
+  dup lodge-allocate throw dup , (lodge-erase) ;
+  \ Compile a lodge variable of _u_ address units.
+
+: lodge-variable ( "name" -- )
+  create cell (lodge-variable) lodge-variable-does> ;
+  \ Create a lodge variable and init it with zero.
+
+: lodge-2variable ( "name" -- )
+  create 2 cells (lodge-variable) lodge-variable-does> ;
+  \ Create a lodge double variable and init it with zero.
+
+\ ==============================================================
 \ Values
 
-: (lodge-value)  ( u -- a )
-  \ Compile a lodge value of u address units.
-  lodge-allocate throw dup , lodge+
-  ;
-: (cell-lodge-value:)  ( x "name" -- )
+: (lodge-value) ( u -- a )
+  lodge-allocate throw dup , lodge+ ;
+  \ Compile a lodge value of _u_ address units.
+
+: (cell-lodge-value:) ( x "name" -- )
+  create  cell (lodge-value) ! ;
   \ Create a 1-cell lodge value.
-  create  cell (lodge-value) !
-  ;
-: lodge-value  ( n "name" -- )
+
+: lodge-value ( n "name" -- )
+  (cell-lodge-value:)  does> ( -- n ) ( dfa ) @ lodge+ @ ;
   \ Create a lodge value.
-  (cell-lodge-value:)  does> ( -- n ) ( dfa ) @ lodge+ @
-  ;
-: lodge-address-value  ( +n "name" -- )
+
+: lodge-address-value ( +n "name" -- )
+  (cell-lodge-value:)  does> ( -- a ) ( dfa ) @ lodge+ @ lodge+ ;
   \ Create a lodge address value.
+  \
   \ +n = lodge offset
+  \
   \ This kind of lodge-value stores a lodge offset but returns the
   \ corresponding absolute address.  This is useful for creating
   \ lodge-values that point to data space in the lodge but have to be
   \ used as ordinary Forth values.
-  (cell-lodge-value:)  does> ( -- a ) ( dfa ) @ lodge+ @ lodge+
-  ;
-: lodge-2value  ( d "name" -- )
-  \ Create a lodge value.
-  create 2 cells (lodge-value) 2!
-  does> ( -- d ) ( dfa ) @ lodge+ 2@
-  ;
 
-\ --------------------------------------------------------------
+: lodge-2value ( d "name" -- )
+  create 2 cells (lodge-value) 2!
+  does> ( -- d ) ( dfa ) @ lodge+ 2@ ;
+  \ Create a lodge value.
+
+\ ==============================================================
 \ To
 
-: xt>lodge  ( xt -- a )
-  \ Absolute lodge address from the xt of a lodge-variable or a
-  \ lodge-value.
-  >body @ lodge+
-  ;
+: xt>lodge ( xt -- a )
+  >body @ lodge+ ;
+  \ Return absolute lodge address _a_ from the _xt_ of a
+  \ lodge-variable or a lodge-value.
 
-: <lodge-to>  ( x "name" -- )
+: <lodge-to> ( x "name" -- )
+  ' xt>lodge ! ;
   \ Change the content of a lodge-variable or a lodge-value.
-  ' xt>lodge !
-  ;
-: [lodge-to]  ( compilation: x "name" -- )
+
+: [lodge-to] ( compilation: x "name" -- )
+  ' postpone literal postpone xt>lodge postpone ! ; immediate
   \ Change the content of a lodge-variable or a lodge-value.
-  ' postpone literal postpone xt>lodge postpone !
-  ; immediate
+
 ' <lodge-to>
 ' [lodge-to]
 interpret/compile: lodge-to
 
-: <lodge-2to>  ( x "name" -- )
+: <lodge-2to> ( x "name" -- )
+  ' xt>lodge 2! ;
   \ Change the content of a lodge-2variable or a lodge-2value.
-  ' xt>lodge 2!
-  ;
-: [lodge-2to]  ( compilation: d "name" -- )
+
+: [lodge-2to] ( compilation: d "name" -- )
+  ' xt>lodge postpone 2literal postpone 2! ; immediate
   \ Change the content of a lodge-2variable or a lodge-2value.
-  ' xt>lodge postpone 2literal postpone 2!
-  ; immediate
+
 ' <lodge-2to>
 ' [lodge-2to]
 interpret/compile: lodge-2to
 
-\ --------------------------------------------------------------
+\ ==============================================================
 \ Misc
 
-: lodge-save-mem  ( a1 len -- +n len )
+: lodge-save-mem ( a1 len -- +n len )
+  swap >r  dup lodge-allocate throw  swap over lodge+ over
+  r> -rot move ;
   \ Copy a memory block into the lodge.
-  \ Written after Gforth's 'save-mem'.
-  swap >r  dup lodge-allocate throw  swap over lodge+ over  r> -rot move
-  ;
-: lodge-,  ( x -- )
-  \ "Compile" a cell into the current lodge.
-  cell lodge-allocate throw lodge+ !
-  ;
-: lodge-2,  ( d -- )
-  \ "Compile" two cells into the current lodge.
-  2 cells lodge-allocate throw lodge+ 2!
-  ;
+  \ Written after Gforth's `save-mem`.
+
+: lodge-, ( x -- )
+  cell lodge-allocate throw lodge+ ! ;
+  \ "Compile" _x_ into the current lodge.
+
+: lodge-2, ( x1 x2 -- )
+  2 cells lodge-allocate throw lodge+ 2! ;
+  \ "Compile" _x1 x2_ into the current lodge.
+
+\ ==============================================================
+\ Change log
+
+\ 2014-02-16 Written for Finto
+\ (http://programandala.net/en.program.finto.html).
+\
+\ 2014-02-17 New: `lodge-value`, `lodge-to` and related words.
+\
+\ 2014-02-19 New: `lodge-address-value`.
+\
+\ 2014-02-20 Change: "index" and "relative pointer" corrected to
+\ "offset" in the comments; additional comment in
+\ `lodge-address-value`; `(cell-lodge-value:)` factored out from
+\ `lodge-value` and `lodge-address-value`; `>lodge` renamed to
+\ `lodge+`.  2014-02-20 New: `lodge-save-mem`.
+\
+\ 2014-02-21 Fix: `[lodge-to]` and `[lodge-2to]` were wrong.
+\
+\  `[lodge-to]`, before the fix:
+\
+\     ' postpone xt>lodge postpone literal postpone !
+\
+\  First bug: `xt>lodge` should not be postponed in this case.
+\  Second bug: the absolute lodge address must not be compiled,
+\  because it may change.
+\
+\   After the fix:
+\
+\     ' postpone literal postpone xt>lodge postpone !
+\
+\ Now the xt is compiled and the lodge address is calculated at
+\ run-time.
+\
+\ 2014-02-21: Forked to <galope/lodge-colon.fs>.
+\
+\ 2014-02-21: New: `lodge-,` and `lodge-2,`.
+\
+\ 2014-02-21: Fix: `lodge-resize` now can be used directly by
+\ the application. Solved by moving the trailing code of
+\ `lodge-allocate` to `lodge-resize`.
+\
+\ 2017-07-03: Update code style. Improve documentation.
+
