c1ee85debdf7963b9c67bca6ad3a1b9730c49f7d
Marcos Cruz <mercurial-repo@programandala.net>
2017-08-15 22:18:18 +0000

    Document `left$`, `mid$`, `right$`.
    FossilOrigin-Name: 14ba75cefd9da775864e7a73abc57c37eb2878c53daaa78bff30b06e33054c9a

--- right-dollar.fs
+++ right-dollar.fs
@@ -1,19 +1,32 @@
 \ galope/right-dollar.fs
-\ right$
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Copyright (C) 2013 Marcos Cruz (programandala.net)
+\ ==============================================================
 
-\ History
+: right$ ( ca1 len1 len2 -- ca2 len3 )
+  over swap - /string ;
+
+  \ doc{
+  \
+  \ right$ ( ca1 len1 len2 -- ca2 len3 )
+  \
+  \ Return a string _ca2 len3_ out of the _len2_ or _len1_ characters
+  \ at the right of string _ca1 len1_ (whichever is smaller).
+  \
+  \ See: `left$`, `mid$`.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
 \ 2013-09-13 Added, after Anton Ertl's examples in <nntp://comp.lang.forth>:
 \   Message-ID: <2013Sep5.105647@mips.complang.tuwien.ac.at>
 \   Newsgroups: comp.lang.forth
 \   Subject: Re: Awk/Forth like mini language, advice
 \   Date: Thu, 05 Sep 2013 08:56:47 GMT
-
-: right$  ( ca1 len1 len2 -- ca' len2 )
-  over swap - /string
-  ;
+\
+\ 2017-08-16: Update source style and file header. Document.
 
