3ebed3f750c915569b421a4132481636aafaf4bd
programandala.net <>
2017-08-15 22:18:18 +0000

    Document `left$`, `mid$`, `right$`.
    FossilOrigin-Name: 14ba75cefd9da775864e7a73abc57c37eb2878c53daaa78bff30b06e33054c9a

--- left-dollar.fs
+++ left-dollar.fs
@@ -4,13 +4,28 @@
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Copyright (C) 2013 Marcos Cruz (programandala.net)
+\ ==============================================================
 
-\ History
+synonym left$ min ( ca1 len1 len2 -- ca1 len3 )
+
+  \ doc{
+  \
+  \ left$ ( ca1 len1 len2 -- ca2 len3 )
+  \
+  \ Return a string _ca2 len3_ out of the _len2_ or _len1_ characters
+  \ at the left of string _ca1 len1_ (whichever is smaller).
+  \
+  \ See: `right$`, `mid$`.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
 \ 2013-09-13 Added, after Anton Ertl's examples in <nntp://comp.lang.forth>:
 \   Message-ID: <2013Sep5.105647@mips.complang.tuwien.ac.at>
 \   Newsgroups: comp.lang.forth
 \   Subject: Re: Awk/Forth like mini language, advice
 \   Date: Thu, 05 Sep 2013 08:56:47 GMT
-
-' min alias left$  ( ca1 len1 len2 -- ca1 len' )
+\
+\ 2017-08-16: Update source style and file header. Document.
--- mid-dollar.fs
+++ mid-dollar.fs
@@ -1,20 +1,31 @@
 \ galope/mid-dollar.fs
-\ mid$
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Copyright (C) 2013 Marcos Cruz (programandala.net)
+\ ==============================================================
 
-\ History
+: mid$  ( ca1 len1 from len2 -- ca2 len3 )
+  >r /string r> min ;
+
+  \ doc{
+  \
+  \ mid$  ( ca1 len1 from len2 -- ca2 len3 )
+  \
+  \ Return a string _ca2 len3_ out of the _len2_ characters
+  \ at the left of string _ca1 len1_, starting at _from_.
+  \
+  \ See: `left$`, `right$`.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
 \ 2013-09-13 Added, after Anton Ertl's examples in <nntp://comp.lang.forth>:
 \   Message-ID: <2013Sep5.105647@mips.complang.tuwien.ac.at>
 \   Newsgroups: comp.lang.forth
 \   Subject: Re: Awk/Forth like mini language, advice
 \   Date: Thu, 05 Sep 2013 08:56:47 GMT
-
-: mid$  ( ca1 len1 from len2 -- ca2 len2 )
-  >r /string r> min
-  ;
-
-
+\
+\ 2017-08-16: Update source style and file header. Document.
