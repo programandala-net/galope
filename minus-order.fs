29ea37b9a40cb30fe952b41adafa39e3406bc41d
Marcos Cruz <mercurial-repo@programandala.net>
2017-08-17 10:30:46 +0000

    Document `-order` and `+order`.
    FossilOrigin-Name: 0bb31c8df909f0eb3bd8db01cc495dab85b187062a30d9916ba62674d0cf2894

--- minus-order.fs
+++ minus-order.fs
@@ -12,11 +12,26 @@
 \ ==============================================================
 
 : -order {: wid -- :}
-  get-order n>r r> dup begin dup while 1-
-    r@ wid = if rdrop -1 under+ else r> -rot then
+  get-order n>r r> dup
+  begin  dup
+  while  1- r@ wid =
+         if   rdrop -1 under+
+         else r> -rot
+         then
   repeat drop set-order ;
+
+  \ doc{
+  \
+  \ -order ( wid -- )
+  \
+  \ Remove all _wid_ from the search order.
+  \
+  \ See: `+order`.
+  \
+  \ }doc
 
 \ ==============================================================
 \ Change log
 
 \ 2017-08-16: Start.
+\ 2017-08-17: Document.
