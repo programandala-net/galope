60c50f6426bef842c573d9ed6c6909294cd6ce33
programandala.net <>
2017-08-17 11:21:32 +0000

    Document `txt+` and `ss+`.
    FossilOrigin-Name: 2ad358c511be6f93036f243e2e7ab236ce85cc12d642dbf7e6d9d33deb6c3e70

--- s-s-plus.fs
+++ s-s-plus.fs
@@ -1,14 +1,9 @@
 \ galope/s-s-plus.fs
-\ `ss+`
-\ Stringer strings concatenator.
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Author: Marcos Cruz (programandala.net), 2016.
-
-\ Description: An operator to concatenate strings and return the
-\ result in the stringer (a circular string buffer).
+\ Author: Marcos Cruz (programandala.net), 2016, 2017.
 
 \ ==============================================================
 
@@ -16,18 +11,31 @@ require ./stringer.fs
 require ./both-lengths.fs
 require ./smove.fs
 
-: ss+  ( ca1 len1 ca2 len2 -- ca3 len3 )
+: ss+ ( ca1 len1 ca2 len2 -- ca3 len3 )
   both-lengths + >r  ( ca1 len1 ca2 len2 ) ( R: len3 )
   r@ allocate-ss >r  ( R: len3 ca3 )
-  2 pick r@ +  ( ca1 len1 ca2 len2 ca3+len1 )  smove  ( ca1 len1 )
+  2 pick r@ +        ( ca1 len1 ca2 len2 ca3+len1 )
+  smove              ( ca1 len1 )
   r@ smove  r> r>  ;
-  \ Concatenate strings _ca1 len1_ and _ca2 len2_ (adding string _ca2
-  \ len2_ at the end of string _ca len1_), returning the result _ca3
-  \ len3_ in the stringer (a circular string buffer).
+
+  \ doc{
+  \
+  \ ss+ ( ca1 len1 ca2 len2 -- ca3 len3 )
+  \
+  \ Concatenate strings _ca1 len1_ and _ca2 len2_, returning the
+  \ result _ca3 len3_ in the `stringer`.
+  \
+  \ See: `txt+`.
+  \
+  \ }doc
 
 \ ==============================================================
-\ History
+\ Change log
 
 \ 2016-07-19: Move from <stringer.fs>.
+\
 \ 2016-07-20: Improve comment.
+\
 \ 2016-07-22: Improve comment.
+\
+\ 2017-08-17: Document. Update header and source style.
