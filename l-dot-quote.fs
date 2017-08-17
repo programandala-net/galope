377bbb74b02e1eb90f0ad1b3d3051d69704ad321
Marcos Cruz <mercurial-repo@programandala.net>
2017-08-17 20:30:18 +0000

    Document left-printing modules, improve word names.
    FossilOrigin-Name: 0f5b29252c120c278aa673a3e8e783ebc403a16f25dc98a243970bc4e80cbe81

--- l-dot-quote.fs
+++ l-dot-quote.fs
@@ -1,25 +1,51 @@
 \ galope/l-dot-quote.fs
-\ Left justified version of `."`
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Author: Marcos Cruz (programandala.net), 2016.
+\ Author: Marcos Cruz (programandala.net), 2016, 2017.
 
-\ 2016-06-22
+\ ==============================================================
 
 require ./l-type.fs
 
 : l."  ( "ccc<quote>" -- )
   postpone s"
-  ['] l-type compile,  ; immediate compile-only
+  ['] ltype compile,  ; immediate compile-only
   \ Parse the input stream until a double quote is found,
   \ and print the parsed string left justified.
 
-: pl."  ( "ccc<quote>" -- )
+  \ doc{
+  \
+  \ l."  ( "ccc<quote>" -- )
+  \
+  \ A variant of ``."`` that displays texts left justifed.
+  \
+  \ See: `/l."`, `ltype`.
+  \
+  \ }doc
+
+: /l."  ( "ccc<quote>" -- )
   postpone s"
-  ['] pl-type compile,  ; immediate compile-only
+  ['] /ltype compile,  ; immediate compile-only
   \ Parse the input stream until a double quote is found,
   \ and print the parsed string left justified, indented
   \ with the value returned by `l-indentation`.
 
+  \ doc{
+  \
+  \ /l."  ( "ccc<quote>" -- )
+  \
+  \ A variant of `l."` that starts a new paragraph.
+  \
+  \ See: `l."`, `/ltype`.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
+\ 2016-06-22: Start.
+\
+\ 2017-08-17: Update to the changes in the <l-type.fs> module. Rename
+\ `pl."` to `/l."`. Document.
