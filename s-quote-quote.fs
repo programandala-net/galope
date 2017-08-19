05ed04f4df626599f247c8568a91b84ee2f32b41
programandala.net <>
2017-08-19 16:25:18 +0000

    Rename `"` to `s""` and change its delimiter. The new name is clearer. Besides, the double delimiter makes highilighting multiline strings tidier, because ordinary strings are not affected.
    FossilOrigin-Name: c7bb6bf0279f2c2967710cc02a351fc529121cae1d1c743fb8fd9947ab86417a

---
+++ s-quote-quote.fs
@@ -0,0 +1,52 @@
+\ galope/s-quote-quote.fs
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2014, 2017.
+
+\ ==============================================================
+
+require ./heredoc.fs
+
+:noname s\" \"\"" (heredoc) save-mem ;
+:noname s\" \"\"" (heredoc) postpone sliteral ;
+interpret/compile: s"" \ ""
+
+  \ doc{
+  \
+  \ s"" \ Interpretation: ( "ccc<space><quote><quote><space>" -- ca len )
+  \     \ Compilation:    ( "ccc<space><quote><quote><space>" -- )
+  \     \ Run-time:       ( -- ca len )
+
+  \ Interpretation:
+  \
+  \ Parse _ccc_ delimited by a word that consists of two double quote.
+  \ Store the resulting string in the heap and return it as _ca len_.
+  \
+  \ Compilation:
+  \
+  \ Parse _ccc_ delimited by a word that consists of two double
+  \ quotes. Append the run-time semantics given below to the current
+  \ definition.
+  \
+  \ Run-time:
+  \
+  \ Return a string _ca len_ that describes the string consisting of
+  \ the characters _ccc_.
+  \
+  \ See: `heredoc`.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
+\ 2014-02-13 Adapted from the word '<"', written by the same author in
+\ 2013 for a text game.
+\
+\ 2017-08-17: Update change log layout. Update source style and
+\ header. Document.
+\
+\ 2017-08-19: Rename `"` to `s""` and use two double quotes as
+\ delimiter; rename the module.  Update documentation.
