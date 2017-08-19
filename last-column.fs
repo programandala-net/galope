00c3d51539415e7d7424c97febb5eae5c4d8ebc2
Marcos Cruz <mercurial-repo@programandala.net>
2017-08-19 21:55:01 +0000

    Fix `last-column`.
    FossilOrigin-Name: a28849a81d1146ead837bf731f227467758c3919bc2a1d5dc211b57a46078800

--- last-column.fs
+++ last-column.fs
@@ -1,12 +1,20 @@
 \ galope/last-column.fs
-\ Last screen column
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
 \ ==============================================================
 
-: last-column ( -- u ) columns 1- ;
+: last-column ( -- n ) cols 1- ;
+
+  \ doc{
+  \
+  \ last-column ( -- n )
+  \
+  \ _n_ is the number of the last screen column, being zero the first
+  \ one.
+  \
+  \ }doc
 
 \ ==============================================================
 \ Change log
@@ -19,3 +27,5 @@
 \
 \ 2017-08-17: Update change log layout. Update header. Update source
 \ style.
+\
+\ 2017-08-19: Fix: Replace `columns` with `cols`.
