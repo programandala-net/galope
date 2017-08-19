ba63131c06f3fc7f9a65faca365e3822c49fbb49
Marcos Cruz <mercurial-repo@programandala.net>
2017-08-19 18:40:13 +0000

    Improve and document `emits`.
    FossilOrigin-Name: d428ce01516181a40c9d491923c62f1c5e16bc65a175a9518020fca8124d4bdc

--- emits.fs
+++ emits.fs
@@ -5,8 +5,15 @@
 
 \ ==============================================================
 
-: emits ( c n -- ) 0 ?do dup emit loop drop ;
-  \ Emit the char c n times.
+: emits ( c n -- ) 0 max 0 ?do dup emit loop drop ;
+
+  \ doc{
+  \
+  \ emits ( c n -- )
+  \
+  \ If _n_ is greater than zero, display the character _c_ _n_ times.
+  \
+  \ }doc
 
 \ ==============================================================
 \ Change log
@@ -15,3 +22,8 @@
 \
 \ 2017-08-17: Update change log layout. Update header. Update source
 \ style.
+\
+\ 2017-08-19: Improve the code after the standard `spaces`: do nothing
+\ if _n_ is negative. This is the code used also by Solo Forth
+\ (http://programandala.net/en.program.solo_forth.html).  Update sorce
+\ style.  Document.
