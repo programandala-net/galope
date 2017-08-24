b8ff86f7e8169b92dd5c9db012e47706d65dc3ec
programandala.net <>
2017-08-24 12:08:15 +0000

    Add `noname-field`.
    FossilOrigin-Name: a14214fe876b2bb633b8cdb4d71f909e18ffe3b4ce0e7fc8ca3c957074d0d206

---
+++ noname-field.fs
@@ -0,0 +1,25 @@
+\ galope/noname-field.fs
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2017.
+
+\ ==============================================================
+
+: noname-field ( -- xt ) noname field: latestxt ;
+
+  \ doc{
+  \
+  \ noname-field ( len1 -- len2 xt )
+  \
+  \ Create an unnamed field, as created by ``field:``, and return its
+  \ _xt_.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
+\ 2017-08-24: Start. Move from _La pistola de agua_
+\ (http://programandala.net/es.programa.la_pistola_de_agua.fs).
