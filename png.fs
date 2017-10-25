84a0f4a95a4c93f586348b8fb31b807c3d6bdeee
programandala.net <>
2017-10-25 16:02:18 +0000

    Improve code and documentation of PNG and JPEG modules.
    FossilOrigin-Name: 0dcdf8d409935686cfa0f211fc02fff956196577ed0e8512fbe85a57ddbf1e60

--- png.fs
+++ png.fs
@@ -12,46 +12,76 @@ require ./buffer-colon.fs
 
 package galope-png
 
-: @le  ( ca -- n )
-  \ Fetch a little-endian 32-bit value.
+: @le ( ca -- n )
   dup c@ [ 0xff 0xff 0xff * * ] literal *
   over 1+ c@ [ 0xff 0xff * ] literal * +
   over 2 + c@ 0xff * +
-  swap 3 + c@ +
-  ;
+  swap 3 + c@ + ;
+  \ Fetch a little-endian 32-bit value.
 
-public
+25 constant /png-buffer
+  \ Size of the PNG buffer, enough to hold the first PNG chunk.
 
-25 constant /png-buffer  \ enough to hold the first PNG chunk
 /png-buffer buffer: png-buffer
 
-: not-png?  ( -- f )
+: not-png? ( -- f )
+  png-buffer @ 0x474e5089 <>
+  png-buffer cell+ @ 0x0a1a0a0d <> or ;
   \ Is the current file not a PNG?
   \ The first bytes in the buffer must be:
   \ 89 50 4E 47 0D 0A 1A 0A
-  png-buffer @ 0x474e5089 <>
-  png-buffer cell+ @ 0x0a1a0a0d <>  or
-  ;
-: png-load  ( fid -- )
-  \ Fill the buffer with the beginning of the PNG image file,
-  \ to make it the current one.
+
+variable png-fid
+
+: png-load ( fid -- )
   png-buffer /png-buffer  2dup erase
   rot read-file throw drop
-  not-png? abort" Not a PNG image file."
-  ;
-variable png-fid
-: png-open  ( ca len -- )
-  \ Open a PNG image file and make it the current one.
-  r/o bin open-file throw  dup png-fid !  png-load
-  ;
-: png-close  ( -- )
-  \ Close the current PNG image.
-  png-fid @ close-file throw
-  ;
-: png-size  ( -- width height )
+  not-png? abort" Not a PNG image file." ;
+  \ Fill the buffer with the beginning of the PNG image file,
+  \ to make it the current one.
+
+public
+
+: png-open ( ca len -- )
+  r/o bin open-file throw dup png-fid ! png-load ;
+
+  \ doc{
+  \
+  \ png-open ( ca len -- )
+  \
+  \ Open a PNG image file _ca len_ to be used by `png-size`.
+  \
+  \ See: `png-close`, `jpeg-open`.
+  \
+  \ }doc
+
+: png-close ( -- )
+  png-fid @ close-file throw ;
+
+  \ doc{
+  \
+  \ png-close ( -- )
+  \
+  \ Close the current PNG file, which was opened by `png-open`.
+  \
+  \ See: `png-size`, `jpeg-open`.
+  \
+  \ }doc
+
+: png-size ( -- width height )
   \ Return the size of the current PNG file.
-  png-buffer 16 + dup @le  swap 4 + @le
-  ;
+  png-buffer 16 + dup @le swap 4 + @le ;
+
+  \ doc{
+  \
+  \ png-size ( -- width height )
+  \
+  \ Return the size _width height_ of the current PNG file, which was
+  \ opened by `png-open`.
+  \
+  \ See: `jpeg-size`.
+  \
+  \ }doc
 
 end-package
 
@@ -74,7 +104,7 @@ end-package
 
 0 [if] \ test code
 
-: png-test  ( c-addr u -- )
+: png-test ( c-addr u -- )
 
   2dup cr type cr
   png-open
@@ -108,3 +138,7 @@ img2 png-test
 \ 2017-08-18: Add missing `;module`.
 \
 \ 2017-08-18: Use `package` instead of `module:`.
+\
+\ 2017-10-25: Reorganize public and private words of the package: Now
+\ only `png-open`, `png-size` and `png-close` are public.  Improve
+\ code layout and documentation.
