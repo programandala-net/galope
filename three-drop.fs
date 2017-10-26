712ae18481d0d03b7c46222f9e14a60278e2e924
Marcos Cruz <mercurial-repo@programandala.net>
2017-10-26 15:19:15 +0000

    Improve documentation.
    FossilOrigin-Name: 259bdd24c9f0efce01e656529322a3ee6b118ffa4476fc3674e17cb6c8d1d2fb

--- three-drop.fs
+++ three-drop.fs
@@ -3,10 +3,21 @@
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
+\ Author: Marcos Cruz (programandala.net), 2012, 2017.
+
 \ ==============================================================
 
 : 3drop ( x1 x2 x3 -- ) 2drop drop ;
-  \  Drop the top three elements from the stack.
+
+  \ doc{
+  \
+  \ 3drop ( x1 x2 x3 -- )
+  \
+  \ Remove cells _x1 x2 x3_ from the stack.
+  \
+  \ See: `4drop`, `3dup`, `third`.
+  \
+  \ }doc
 
 \ ==============================================================
 \ Change log
@@ -15,3 +26,5 @@
 \
 \ 2017-08-17: Update change log layout. Update header and source
 \ style.
+\
+\ 2017-10-26: Improve documentation.
