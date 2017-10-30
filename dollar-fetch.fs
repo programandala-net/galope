decacdecccf5a42b418a57b37b40251b89a43d20
programandala.net <>
2017-10-30 15:20:46 +0000

    Improve and document `$!`.
    FossilOrigin-Name: 8f111173964780cb2ff063893bb5fb5814140e0bc8fbe81024c5fdb7544a647c

--- dollar-fetch.fs
+++ dollar-fetch.fs
@@ -1,5 +1,4 @@
 \ galope/dolar-fetch.fs
-\ Alternative definiton for Gforth's '$@'
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
@@ -8,18 +7,29 @@
 
 \ ==============================================================
 
-s" gforth" environment? drop s" 0.8" str< [if]
+s" gforth" environment? drop s" 0.7.9" str< [if]
 
-  \ Gforth < 0.8
+  \ Gforth < 0.7.9
 
   require string.fs  \ Gforth's dynamic strings
 
   warnings @  warnings off
 
   : $@ ( a -- ca len )
-    @ dup if dup cell+ swap @ else pad swap then ;
-    \ Return the content of a dynamic string variable,
-    \ even if it's not initialized.
+    @ dup if dup cell+ swap @ else 0 then ;
+
+  \ doc{
+  \
+  \ $@ ( a -- ca len )
+  \
+  \ Return the string _ca len_ stored at a dynamic string variable
+  \ _a_, even if the variable has not been initialized.
+  \
+  \ ``$@`` is already provided by Gforth. This is an improved
+  \ definition. But Gforth 0.7.9 already includes this improvement.
+  \ Therefore this word is not loaded on Gforth >=0.7.9.
+  \
+  \ }doc
 
   warnings !
 
@@ -33,3 +43,6 @@ s" gforth" environment? drop s" 0.8" str< [if]
 \ 2015-02-11: Gforth 0.8 will work this way, so a check is added.
 \
 \ 2017-08-17: Update change log layout and source style.
+\
+\ 2017-10-30: Check Gforth 0.7.9, which works this way. Don't use
+\ `pad`.
