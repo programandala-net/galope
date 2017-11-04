7d5ebd06d100ffb0b6a2ee59a12c4d9a4d089454
programandala.net <>
2017-11-04 17:15:17 +0000

    Improve documentation.
    FossilOrigin-Name: cb87b25f6098f6446a64fc8c419f3a0a6c573a1f9e9b37ad25ce792883e0b0e9

--- question-nip.fs
+++ question-nip.fs
@@ -1,5 +1,4 @@
 \ galope/question-nip.fs
-\ ?nip
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
@@ -8,9 +7,15 @@
 
 \ ==============================================================
 
-: ?nip ( false | x non-false -- f ) dup if nip then ;
-  \ Discard the second element on the stack if top of stack is not
-  \ false. Leave the top of stack unchanged.
+: ?nip ( x f -- f | x f ) dup if nip then ;
+
+  \ doc{
+  \
+  \ ?nip ( x f -- f | x f )
+  \
+  \ If _f_ is zero, do nothing.  Otherwise drop _x_.
+  \
+  \ }doc
 
 \ ==============================================================
 \ Change log
@@ -18,3 +23,5 @@
 \ 2013-12-31 Added.
 \
 \ 2017-08-17: Update change log layout and source style.
+\
+\ 2017-11-04: Fix stack comment. Improve documentation.
--- question-nullify.fs
+++ question-nullify.fs
@@ -1,20 +1,28 @@
 \ galope/question-nullify.fs
-\ `?nullify`
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Author: Marcos Cruz (programandala.net), 2016.
+\ Author: Marcos Cruz (programandala.net), 2016, 2017.
 
 \ ==============================================================
 
 require random.fs  \ Gforth's `random`
 
-: ?nullify  ( x n -- x | 0 )  random 0= abs *  ;
+: ?nullify ( x n -- x | 0 ) random 0= abs * ;
+
+  \ doc{
+  \
+  \ ?nullify ( x n -- x | 0 )
+  \
   \ Randomly return zero instead of _x_, one choice out of _n_.
+  \
+  \ }doc
 
 \ ==============================================================
 \ Change log
 
 \ 2016-07-19: Write. A generalization of the old `s?`, included in the
 \ deprecated module <sb.fs>.
+\
+\ 2017-11-04: Improve documentation.
