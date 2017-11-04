4c3fb201269a4a02c9f25f174bc7c52604dd2ae8
programandala.net <>
2017-11-04 17:46:56 +0000

    Update module names after the current convention.
    FossilOrigin-Name: 6841fa8755dad936ce7ba214f0b22363746dd4d6661138ac98737eac0b21caf9

--- sconstant.fs
+++ sconstant.fs
@@ -1,23 +1,16 @@
 \ galope/sconstant.fs
-\ String constant
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Author: Marcos Cruz (programandala.net), 2012.
+\ Author: Marcos Cruz (programandala.net), 2017.
 
 \ ==============================================================
 
-: sconstant (  ca len "name" -- )
-  create s,
-  does> ( -- ca len ) ( dfa ) count ;
+require ./s-constant.fs
 
 \ ==============================================================
 \ Change log
 
-\ 2012-05-01: Extracted from and application of the author.
-\
-\ 2014-11-02: Change: Stack comments.
-\
-\ 2017-08-17: Update change log layout. Update source style and stack
-\ notation.
+\ 2017-11-04: Added for backward compatibility with the old name,
+\ until the projects that use this module are updated.
