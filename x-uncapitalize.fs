b3f138c89e85595ceed12422e83c525c1fb78dcd
programandala.net <>
2017-11-08 14:58:03 +0000

    Add `xuncapitalize` and `xuncapitalized`.
    FossilOrigin-Name: 50c2ea2ec67649cae794aca3ae28ad8d9c1357d4bad160f9650f328c8bf61e30

---
+++ x-uncapitalize.fs
@@ -0,0 +1,30 @@
+\ galope/x-uncapitalize.fs
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2017.
+
+\ ==============================================================
+
+require ./x-c-store.fs
+require ./xcase.fs
+
+: xuncapitalize ( xca len -- )
+  if dup xc@ xtolower swap xc! else drop then ;
+
+  \ doc{
+  \
+  \ xuncapitalize ( xca len -- )
+  \
+  \ Convert the first character of UTF-8 character string _xca len_ to
+  \ lowercase.
+  \
+  \ See: `xuncapitalized`, `xcapitalize`.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
+\ 2017-11-08: Start.
