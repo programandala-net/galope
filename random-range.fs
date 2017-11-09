58189e61d763c016c02ef40b920e3fd0f7328faa
programandala.net <>
2017-11-09 20:03:21 +0000

    Remove `/random/`, keep `random-range`; improve documentation.
    FossilOrigin-Name: 074609b2dd40f14a5a2a5f0f481a6acaf4ef8e0ee9e1a8991e8b5e23d227b84b

--- random-range.fs
+++ random-range.fs
@@ -1,22 +1,33 @@
 \ galope/random-range.fs
-\ Random number in a range.
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Author: Marcos Cruz (programandala.net), 2014.
+\ Author: Marcos Cruz (programandala.net), 2013, 2014, 2017.
 
 \ ==============================================================
 
 require random.fs
 
-: random-range ( min max -- n )
-  over - 1+ random + ;
-  \ Return a random number from min to max.
+: random-range ( n1 n2 -- n3 ) over - 1+ random + ;
+
+  \ doc{
+  \
+  \ random-range ( n1 n2 -- n3 )
+  \
+  \ Return a random number _n_ from _n1_ (minimum) to _n2_ (maximum).
+  \
+  \ See: `randomize`.
+  \
+  \ }doc
 
 \ ==============================================================
 \ Change log
 
-\ 2014-05-22: Written.
+\ 2013-08-16: Written as `/random/`.
+\
+\ 2014-05-22: Rewritten and improved as `random-range`.
 \
 \ 2017-08-17: Update change log layout. Update source style.
+\
+\ 2017-11-09: Improve documentation.
--- randomize.fs
+++ randomize.fs
@@ -1,5 +1,4 @@
 \ galope/randomize.fs
-\ Change the random seed
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
@@ -12,9 +11,22 @@ require random.fs  \ Gforth's 'random'
 
 : randomize ( -- ) utime * seed ! ;
 
+  \ doc{
+  \
+  \ randomize ( -- )
+  \
+  \ Set the ``random``'s ``seed`` using the values returned by
+  \ ``utime``.
+  \
+  \ See: `random-range`.
+  \
+  \ }doc
+
 \ ==============================================================
 \ Change log
 
 \ 2013-08-30 Change: 'utime' instead of 'time&date'.
 \
 \ 2017-08-17: Update change log layout and source style.
+\
+\ 2017-11-09: Improve documentation.
