7b2955c20ce5d8550ea5925654b195f70317b531
Marcos Cruz <mercurial-repo@programandala.net>
2017-11-14 14:35:30 +0000

    Add `warning(`.
    FossilOrigin-Name: df82a77408a2d9aed72e069cd796d886a58adaa686af65bc7508b6f0068dc056

---
+++ warning-paren.fs
@@ -0,0 +1,26 @@
+\ galope/warning-paren.fs
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2017.
+
+\ ==============================================================
+
+: warning( ( "ccc<paren>" -- )
+  ')' parse true ['] type ?warning ; immediate
+
+  \ doc{
+  \
+  \ warning( ( "ccc<paren>" -- )
+  \
+  \ Parse _ccc_ delimited by ')' and display _ccc_ as a warning.
+  \
+  \ ``warning(`` is an immediate word.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
+\ 2017-11-14: Start.
