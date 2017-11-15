619482d41111f8c99e189af0cee5027385391ee2
Marcos Cruz <mercurial-repo@programandala.net>
2017-11-15 19:29:33 +0000

    Add `name>flags`, `immediate?`, `compile-only?`.
    FossilOrigin-Name: 0acf88388dac067a909fb96152debb0395c7f4b49f4ae5686c87345b246f5d24

---
+++ immediate-question.fs
@@ -0,0 +1,34 @@
+\ galope/immediate-question.fs
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programanda.net), 2017.
+
+\ ==============================================================
+
+[undefined] immediate? [if]
+
+\ Gforth<0.7.9
+
+require ./name-to-flags.fs
+
+: immediate? ( nt -- f )
+  name>flags @ immediate-mask and 0<> ;
+
+  \ doc{
+  \
+  \ immediate? ( nt -- f )
+  \
+  \ _f_ is true if the word _nt_ is immediate.
+  \
+  \ See: `compile-only?`, `name>flags`.
+  \
+  \ }doc
+
+[then]
+
+\ ==============================================================
+\ Change log
+
+\ Start.
---
+++ name-to-flags.fs
@@ -0,0 +1,27 @@
+\ galope/name-to-flags.fs
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programanda.net), 2017.
+
+\ ==============================================================
+
+: name>flags ( nt -- a )
+  [ s" gforth" environment? drop s" 0.7.9" str< ]
+  [if] cell+ [else] >f+c [then] ;
+
+  \ doc{
+  \
+  \ name>flags ( nt -- a|ca )
+  \
+  \ Convert _nt_ to the address of the flags byte _ca_.
+  \
+  \ See: `immediate?`, `compile-only?`.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
+\ Start.
