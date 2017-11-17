80291f293ce19216d1de90503186213bad001bb6
Marcos Cruz <mercurial-repo@programandala.net>
2017-11-17 21:26:57 +0000

    Fix `this-lprompt`.
    FossilOrigin-Name: 7cc37042abea174f57e27b768126df5badce8ba82e04fad26a1adf85dded5c69

--- l-type.fs
+++ l-type.fs
@@ -7,15 +7,15 @@
 \
 \ Left justified version of `type`.
 
-\ Author: Marcos Cruz (programandala.net), 2012, 2013, 2014, 2015,
-\ 2016, 2017.
+\ Author: Marcos Cruz (programandala.net), 2012, 2013, 2014,
+\ 2015, 2016, 2017.
 
 \ Credits:
 \
 \ This code was inspired by:
 \ 4tH library - PRINT - Copyright 2003,2010 J.L. Bezemer
 
-\ Last modified 201711171328
+\ Last modified 201711172219
 \ See change log at the end of the file.
 
 \ ==============================================================
@@ -71,15 +71,14 @@ variable lmore lmore off
   \
   \ lmore ( -- a )
   \
-  \ _a_ is the address of a cell containing a flag, whose default
-  \ value is _false_. When ``lmore`` is _true_, the left-justified
-  \ diplaying system displays a prompt when the screen is full.
+  \ _a_ is the address of a cell containing a flag, whose
+  \ default value is _false_. When ``lmore`` is _true_, the
+  \ left-justified diplaying system displays a prompt when the
+  \ screen is full.
   \
   \ See: `lprompt`, `ltype`.
   \
   \ }doc
-
-  \ Flag: Show a prompt when the screen is full?
 
 : (lprompt)$ ( -- ca len ) s" ..." ;
 
@@ -87,9 +86,9 @@ variable lmore lmore off
   \
   \ (lprompt)$ ( -- ca len ) s" ..." ;
   \
-  \ _ca len_ is the default prompt used by the `ltype` displaying system
-  \ when the screen is full.  ``(lprompt)$`` is the default action
-  \ of `lprompt$`.
+  \ _ca len_ is the default prompt used by the `ltype`
+  \ displaying system when the screen is full.  ``(lprompt)$``
+  \ is the default action of `lprompt$`.
   \
   \ }doc
 
@@ -99,9 +98,9 @@ defer lprompt$ ( -- ca len )
   \
   \ lprompt$ ( -- ca len )
   \
-  \ _ca len_ is the prompt used by the `ltype` displaying system when
-  \ the screen is full. ``lprompt$``is a deferred word whose
-  \ default action is `(lprompt)$`.
+  \ _ca len_ is the prompt used by the `ltype` displaying
+  \ system when the screen is full. ``lprompt$``is a deferred
+  \ word whose default action is `(lprompt)$`.
   \
   \ }doc
 
@@ -125,9 +124,9 @@ defer lprompt$ ( -- ca len )
   \
   \ indentation1 ( -- n )
   \
-  \ Return the number _n_ of spaces at the left of the first line of a
-  \ new paragraph. ``indentation1`` is a ``value``. Its default value
-  \ is 2.
+  \ Return the number _n_ of spaces at the left of the first
+  \ line of a new paragraph. ``indentation1`` is a ``value``.
+  \ Its default value is 2.
   \
   \ See `indentation2`, `/paragraph`.
   \
@@ -149,9 +148,9 @@ synonym indentation indentation1 \ XXX OLD -- for retrocompatibility
   \
   \ indentation2 ( -- n )
   \
-  \ Return the number _n_ of spaces at the left of the second and
-  \ following lines of a new paragraph. ``indentation2`` is a
-  \ ``value``.  Its default value is 0.
+  \ Return the number _n_ of spaces at the left of the second
+  \ and following lines of a new paragraph. ``indentation2`` is
+  \ a ``value``.  Its default value is 0.
   \
   \ See `indentation1`, `/paragraph`.
   \
@@ -163,11 +162,11 @@ variable indent-top
   \
   \ indent-top ( -- a )
   \
-  \ _a_ is the address of a cell containing a flag. If the flag is
-  \ _true_, the first line of a paragraph is indented even at the top
-  \ of the page, ie. at row zero. If the flag is _false_ (the default
-  \ value), the first line of a paragraph is not indented at the top
-  \ of the page.
+  \ _a_ is the address of a cell containing a flag. If the flag
+  \ is _true_, the first line of a paragraph is indented even
+  \ at the top of the page, ie. at row zero. If the flag is
+  \ _false_ (the default value), the first line of a paragraph
+  \ is not indented at the top of the page.
   \
   \ See: `indent`, `indentation1`.
   \
@@ -190,7 +189,8 @@ public
   \ indent ( -- )
   \
   \ Indent the proper number of spaces (`indentation1` or
-  \ `indentation2`) depending on the current line of the paragraph.
+  \ `indentation2`) depending on the current line of the
+  \ paragraph.
   \
   \ See: `?indent`.
   \
@@ -215,7 +215,8 @@ public
   \
   \ lspace ( -- )
   \
-  \ Display a space as part of the left-justified displaying system.
+  \ Display a space as part of the left-justified displaying
+  \ system.
   \
   \ See: `lemit`, `ltype`.
   \
@@ -227,8 +228,8 @@ public
   \
   \ lhome ( -- )
   \
-  \ Move the cursor used by `ltype` and related words to its home
-  \ position, at the top left (column 0, row 0).
+  \ Move the cursor used by `ltype` and related words to its
+  \ home position, at the top left (column 0, row 0).
   \
   \ }doc
 
@@ -250,7 +251,8 @@ rows 2 - value lrows ( -- n )
   \ lrows ( -- n )
   \
   \ _n_ is the number of rows per page displayed by `ltype`.
-  \ ``lrows`` is a ``value`` whose default content is ``rows 2 -``.
+  \ ``lrows`` is a ``value`` whose default content is ``rows 2
+  \ -``.
   \
   \ }doc
 
@@ -260,9 +262,9 @@ rows 2 - value lrows ( -- n )
   \
   \ lcr? ( -- f )
   \
-  \ Is the cursor to the next row neither at the home position nor at
-  \ the start of a line?  ``lcr?`` is part of the left-justified
-  \ displaying system.
+  \ Is the cursor to the next row neither at the home position
+  \ nor at the start of a line?  ``lcr?`` is part of the
+  \ left-justified displaying system.
   \
   \ See: `lcr`, `ltype`.
   \
@@ -297,28 +299,29 @@ defer lprompt-pause ( -- ) ' (lprompt-pause) is lprompt-pause
   \
   \ lprompt-pause ( i*x -- )
   \
-  \ A deferred word used by `this-lprompt`, which is part of the
-  \ `ltype` system; its default action is `(lprompt-pause)`.  _i*x_ is
-  \ a possible optional parameter needed by alternative actions.
+  \ A deferred word used by `this-lprompt`, which is part of
+  \ the `ltype` system; its default action is
+  \ `(lprompt-pause)`.  _i*x_ is a possible optional parameter
+  \ needed by alternative actions.
   \
   \ }doc
 
 : this-lprompt ( i*x ca len -- )
   xy    2>r dup >r type lprompt-pause r>
-        2r> at-xy spaces
-        2r@ at-xy
+        2r@ at-xy spaces
+        2r> at-xy
   lrows# off ;
 
   \ doc{
   \
   \ this-lprompt ( i*x ca len -- )
   \
-  \ Display prompt _ca len_, execute `lprompt-pause` with optional
-  \ parameters _i*x_ and remove the prompt restoring the original
-  \ position of the cursor.
+  \ Display prompt _ca len_, execute `lprompt-pause` with
+  \ optional parameters _i*x_ and remove the prompt restoring
+  \ the original position of the cursor.
   \
-  \ ``this-lprompt`` is a factor of `lprompt`, which is part of the
-  \ `ltype` displaying system.
+  \ ``this-lprompt`` is a factor of `lprompt`, which is part of
+  \ the `ltype` displaying system.
   \
   \ }doc
 
@@ -340,10 +343,11 @@ defer ((lcr)) ( -- ) ' cr is ((lcr))
   \
   \ ((lcr)) ( -- )
   \
-  \ A deferred word whose default action is ``cr``.  This is the
-  \ actual carriage return done by `(lcr)`, before updating the data
-  \ of the left-justified displaying system.  ``((lcr))`` is a hook
-  \ for the application, for special cases.
+  \ A deferred word whose default action is ``cr``.  This is
+  \ the actual carriage return done by `(lcr)`, before updating
+  \ the data of the left-justified displaying system.
+  \ ``((lcr))`` is a hook for the application, for special
+  \ cases.
   \
   \ }doc
 
@@ -368,8 +372,8 @@ defer ((lcr)) ( -- ) ' cr is ((lcr))
   \
   \ lcr ( -- )
   \
-  \ If needed, move the cursor to the next row, ``lcr`` is part of the
-  \ left-justified displaying system.
+  \ If needed, move the cursor to the next row, ``lcr`` is part
+  \ of the left-justified displaying system.
   \
   \ See: `lcr?`, `(lcr)`, `ltype`.
   \
@@ -381,9 +385,9 @@ variable lwidth cols lwidth !
   \
   \ lwidth ( -- a )
   \
-  \ A variable containing the text width in columns used by `ltype`
-  \ and related words. Its default value is ``cols``, ie. the current
-  \ width of the screen.
+  \ A variable containing the text width in columns used by
+  \ `ltype` and related words. Its default value is ``cols``,
+  \ ie. the current width of the screen.
   \
   \ }doc
 
@@ -419,8 +423,8 @@ public
   \
   \ /paragraph ( -- n )
   \
-  \ Blank rows between paragraphs. ``/paragraph`` is a ``value`` whose
-  \ default value is 1.
+  \ Blank rows between paragraphs. ``/paragraph`` is a
+  \ ``value`` whose default value is 1.
   \
   \ See: `indentation1`, `indentation2`, `paragraph`.
   \
@@ -475,37 +479,38 @@ require ./n-to-str.fs
 
 \ 2012-04-17: Words renamed and factorized. Adapted to Gforth.
 \
-\ 2012-04-18: Added 'module.fs'. Name fixed. Added 'xy.fs'.  First
-\ working version.
+\ 2012-04-18: Added 'module.fs'. Name fixed. Added 'xy.fs'.
+\ First working version.
 \
-\ 2012-05-01: Fix: '?cr' now checks not only the row, but the column.
-\ Second, experimental version with 'column'.
+\ 2012-05-01: Fix: '?cr' now checks not only the row, but the
+\ column.  Second, experimental version with 'column'.
 \
-\ 2012-05-02: '?cr' removed; 'print_cr' and 'unhome?' used instead.
-\ New words 'print_x+' and 'print_indentation' provide the low level
-\ interface for indentation.
+\ 2012-05-02: '?cr' removed; 'print_cr' and 'unhome?' used
+\ instead.  New words 'print_x+' and 'print_indentation'
+\ provide the low level interface for indentation.
 \
-\ 2012-05-07: 'print_indentation' moves the cursor instead of printing
-\ spaces (that changed the background color); the trm module from the
-\ Free Foundation Library is used.
+\ 2012-05-07: 'print_indentation' moves the cursor instead of
+\ printing spaces (that changed the background color); the trm
+\ module from the Free Foundation Library is used.
 \
-\ 2012-05-08: Exported 'no_printed'; added 'print_home' (a proper
-\ 'home').  'unhome?' renamed 'not_at_home?'.  New version of
-\ 'print_start_of_line', based on Forth Foundation Library's trm
-\ module.
+\ 2012-05-08: Exported 'no_printed'; added 'print_home' (a
+\ proper 'home').  'unhome?' renamed 'not_at_home?'.  New
+\ version of 'print_start_of_line', based on Forth Foundation
+\ Library's trm module.
 \
-\ 2012-05-17: Removed the deprecated alternative slow version.  Now
-\ 'cr' is defered by '(print_cr)', what makes it possible to achive
-\ some special effects in the application, e.g. coloring the new lines
-\ at the bottom of the screen.
+\ 2012-05-17: Removed the deprecated alternative slow version.
+\ Now 'cr' is defered by '(print_cr)', what makes it possible
+\ to achive some special effects in the application, e.g.
+\ coloring the new lines at the bottom of the screen.
 \
-\ 2012-09-29: Fixed: a 'hide' was missing. A check was missing in
-\ 'print_indentation' because the FFL's 'trm+move-cursor-right' moves
-\ the cursor one column when the parameter is 1! The same happens with
-\ 'trm+move-cursor-left'. The author of FFL has been contacted.
+\ 2012-09-29: Fixed: a 'hide' was missing. A check was missing
+\ in 'print_indentation' because the FFL's
+\ 'trm+move-cursor-right' moves the cursor one column when the
+\ parameter is 1! The same happens with 'trm+move-cursor-left'.
+\ The author of FFL has been contacted.
 \
-\ 2012-09-30: Fixed: 'at_last_start_of_line?' used the coordinates in
-\ the wrong order.
+\ 2012-09-30: Fixed: 'at_last_start_of_line?' used the
+\ coordinates in the wrong order.
 \
 \ 2012-11-30: Fixed: 'print_cr?'.
 \
@@ -522,41 +527,44 @@ require ./n-to-str.fs
 \ ----------------------------------------------
 \ Current version, as <l-type.fs>
 
-\ 2016-06-22: Forked from the `print` module, in order to improve it
-\ and replace it gradually, without breaking existing code. Rename:
-\ Replace underscores with hyphens. New main interface words: `l-type`
-\ and `pl-type`. General renaming.
+\ 2016-06-22: Forked from the `print` module, in order to
+\ improve it and replace it gradually, without breaking
+\ existing code. Rename: Replace underscores with hyphens. New
+\ main interface words: `l-type` and `pl-type`. General
+\ renaming.
 \
 \ 2017-07-14: Fix typo.
 \
-\ 2017-08-17: Update stack notation.  Change "l-" prefix to "l". The
-\ name of the module remains "l-type", because it reflects the
-\ pronunciation of the word, after the convention used in the library.
-\ Update source style. Remove `l-start-of-line` and
-\ `at-last-start-of-line`, which were unnecessary. Rename `pl-type` to
-\ `/ltype`. Rename `rows-between-paragraphs` to `/paragraph`. Document
-\ the most importart public words of the module.
+\ 2017-08-17: Update stack notation.  Change "l-" prefix to
+\ "l". The name of the module remains "l-type", because it
+\ reflects the pronunciation of the word, after the convention
+\ used in the library.  Update source style. Remove
+\ `l-start-of-line` and `at-last-start-of-line`, which were
+\ unnecessary. Rename `pl-type` to `/ltype`. Rename
+\ `rows-between-paragraphs` to `/paragraph`. Document the most
+\ importart public words of the module.
 \
 \ 2017-08-18: Use `package` instead of `module:`.
 \
 \ 2017-09-08: Replace `/word` with `/name`, whose definition is
-\ identical and is part of the library already. Simplify `ltype` with
-\ `/first-name`. Rename all "typed" to "ltyped".
+\ identical and is part of the library already. Simplify
+\ `ltype` with `/first-name`. Rename all "typed" to "ltyped".
 \
 \ 2017-10-11: Add missing requirement `/first-name`.
 \
-\ 2017-10-22: Add indentation for the second and following lines.
-\ Rename `l-width` `lwidth` and document it. Rename
+\ 2017-10-22: Add indentation for the second and following
+\ lines.  Rename `l-width` `lwidth` and document it. Rename
 \ `current-print-width` `width`. Rename `too-long?` `unfit?`.
 \
 \ 2017-10-24: Improve documentation.
 \
 \ 2017-11-03: Improve change log.
 \
-\ 2017-11-16: Start implementation of a configurable "more?" control.
+\ 2017-11-16: Start implementation of a configurable "more?"
+\ control.
 \
-\ 2017-11-17: Improve factoring, names and documentation. Update
-\ requirements. Add flag `indent-top`, `lprompt-pause`, and others.
-\ Expand the public interface.  Fix `paragraph`: do not separate the
-\ new paragraph if cursor is at the top left corner of the screen.
-\ Remove GPL license.
+\ 2017-11-17: Improve factoring, names and documentation.
+\ Update requirements. Add flag `indent-top`, `lprompt-pause`,
+\ and others.  Expand the public interface.  Fix `paragraph`:
+\ do not separate the new paragraph if cursor is at the top
+\ left corner of the screen.  Remove GPL license.
