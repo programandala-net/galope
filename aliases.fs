4865ad338e322590e699f80405fa28e3a712ca3b
Marcos Cruz <mercurial-repo@programandala.net>
2017-12-04 11:42:11 +0000

    Rename `aliases:`, `immediate-aliases:` and ";aliases" (a parsed marked). New names: `aliases`, `immediate-aliases` and "end-aliases".
    FossilOrigin-Name: 824731bb646a8011691beeee188c51e3a6166684ddced9f6d71e2a9ddf471563

--- aliases.fs
+++ aliases.fs
@@ -1,5 +1,4 @@
-\ aliases-colon.fs
-\ `aliases:`
+\ aliases.fs
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
@@ -11,34 +10,37 @@
 require ./colon-alias.fs
 require ./package.fs
 
-package galope-aliases-colon
+package galope-aliases
 
 : parse-alias  ( -- ca len )
-  begin   parse-name dup 0=
-  while   2drop refill 0=
-          abort" Error: `;aliases` missing"
-  repeat  ;
+  begin  parse-name dup 0=
+  while  2drop refill 0=
+         abort" Error: `end-aliases` is missing"
+  repeat ;
   \ Parse and return the next alias of the list started by
-  \ `aliases{`.
+  \ `aliases`.
 
 : another-alias? ( -- ca len f )
-  parse-alias 2dup s" ;aliases" compare ;
+  parse-alias 2dup s" end-aliases" compare ;
   \ Parse and return the next alias _ca len_ of the list started by
-  \ `aliases{`. If _f_ is false, _ca len_ is the word "}aliases",
+  \ `aliases`. If _f_ is false, _ca len_ is the word "end-aliases",
   \ which marks the end of the list.
 
 public
 
-: aliases: (  xt "name#0" ... "name#n" ";aliases" -- )
+: aliases ( xt "name#0" .. "name#n" "end-aliases" -- )
   begin  dup another-alias? ( xt xt ca len f )
   while  :alias
   repeat 2drop 2drop ;
 
   \ doc{
   \
-  \ aliases: (  xt "name#0" ... "name#n" ";aliases" -- )
+  \ aliases ( xt "name#0" .. "name#n" "end-aliases" -- )
   \
-  \ Create any number of aliases of _xt_, until ";aliases" is parsed.
+  \ Create any number of aliases of _xt_, until "end-aliases" is
+  \ parsed.
+  \
+  \ See: `immediate-aliases`, `:alias`.
   \
   \ }doc
 
@@ -58,3 +60,6 @@ end-package
 \ 2017-08-18: Use `package` instead of `module:`.
 \
 \ 2017-10-24: Improve documentation.
+\
+\ 2017-12-04: Rename `aliases:` `aliases`; rename `;aliases`
+\ `end-aliases`. Improve documentation.
