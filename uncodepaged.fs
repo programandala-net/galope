397bb34adcb961c4d4791841eb8ae0d12eeb5735
Marcos Cruz <mercurial-repo@programandala.net>
2018-01-20 11:41:19 +0000

    Improve documentation and source style.
    FossilOrigin-Name: 7f1d6fe9558be39350b16bce432b80427a753ea3d659bc136e76a32f1a7f04ec

--- uncodepaged.fs
+++ uncodepaged.fs
@@ -1,9 +1,10 @@
 \ galope/uncodepaged.fs
-\ Tool to transform a string,
-\ converting certain of its 8-bit chars to strings.
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
+
+\ Description: Tool to transform a string, replacing 8-bit
+\ characters with strings.
 
 \ Author: Marcos Cruz (programandala.net), 2013, 2017.
 
@@ -11,6 +12,7 @@
 \ Requirements
 
 require ./package.fs
+
 require ffl/str.fs
 
 \ ==============================================================
@@ -19,86 +21,81 @@ require ffl/str.fs
 package galope-uncodepaged
 
 str-create uncodepaged-str
+
 variable depth0
+
 3 constant stack/translation
+
 2 cells constant /translation
+
 256 constant chars/table
+
 chars/table /translation * constant /table
+
 variable 'table  \ address of the translation table just defined
-: erase_table  ( a -- )
+
+: erase_table  ( a -- ) /table erase ;
   \ Erase a translation table.
-  /table erase
-  ;
-: new_table  ( -- a )
+
+: new_table  ( -- a ) /table allocate throw  dup erase_table ;
   \ Create a new translation table.
-  /table allocate throw  dup erase_table
-  ;
-: #translations  ( c1 ca1 len1 ... c'n ca'n len'n -- c1 ca1 len1 ... c'n ca'n len'n n )
-  \ Calculate the number of translations defined in the translation table.
-  depth depth0 @ - stack/translation /
-  ;
-: 'translation  ( a1 c -- a2 )
+
+: #translations \ ( c#1 ca#1 len#1 .. c#n ca#n len#n --
+                \   c#1 ca#1 len#1 .. c#n ca#n len#n n )
+  depth depth0 @ - stack/translation / ;
+  \ Calculate the number of translations defined in the
+  \ translation table.
+
+: 'translation  ( a1 c -- a2 ) /translation * + ;
   \ Return the address of a translation in a translation table.
   \ a1 = address of the translation table
   \ c = character of the translation table
   \ a2 = addres of the translation
-\  ." 'translation " cr .s cr key drop  \ xxx informer
-\  ." 'table " 'table ? cr key drop  \ xxx informer
-  /translation * +
-  ;
-: translation!  ( ca len a c -- )
+
+: translation!  ( ca len a c -- ) 'translation 2! ;
   \ Store a string into a translation table.
   \ ca len = string
   \ a = address of the translation table
   \ c = character to store into
-  'translation 2!
-  ;
+
 : translation,  ( c ca len -- )
+  rot 'table @ swap translation! ;
   \ Compile a translation into the translation table.
-  rot 'table @ swap translation!
-  ;
+
 : translations,  ( c'1 ca'1 len'1 ... c'n ca'n len'n n -- )
+  0 ?do translation, loop ;
   \ Compile the translations into the translation table.
   \ n = number of translations
-\  .s cr key drop  \ xxx informer
-  0 ?do  translation,  loop
-  ;
-: translation@  ( a c -- ca len | 0 0 )
+
+: translation@  ( a c -- ca len | 0 0 ) 'translation 2@ ;
   \ Fetch a translation from a translation table
   \ (or two zeros if no translation has been defined for the character).
   \ a = address of the translation table
   \ c = character to translate
   \ ca len = translation
-  'translation 2@
-  ;
-: translation@?  ( a c -- ca len f )
+
+: translation@?  ( a c -- ca len f ) translation@ 2dup + ;
   \ Fetch a translation from a translation table.
   \ a = address of the translation table
   \ c = character to translate
   \ ca len = translation
   \ f = valid translation?
-  translation@ 2dup +
-  ;
+
 : string>uncodepaged  ( ca len -- )
+  uncodepaged-str str-append-string ;
   \ Append a string to the uncodepaged string.
-\  ." string>uncodepaged START" cr .s cr key drop  \ xxx informer
-  uncodepaged-str str-append-string
-  ;
+
 : char>uncodepaged  ( c -- )
+  1 uncodepaged-str str-append-chars ;
   \ Append a character to the uncodepaged string.
-\  ." char>uncodepaged START" cr .s cr key drop  \ xxx informer
-  1 uncodepaged-str str-append-chars
-  ;
+
 : >uncodepaged  ( a c -- )
+  dup >r translation@? r> swap
+  if   drop string>uncodepaged
+  else char>uncodepaged 2drop then ;
   \ Append a translation to the uncodepaged string.
   \ a = address of the translation table
   \ c = character whose translation to add
-\  ." >uncodepaged START" cr .s cr key drop  \ xxx informer
-  dup >r translation@? r> swap
-\  ." >uncodepaged BEFORE IF" cr .s cr key drop  \ xxx informer
-  if    drop string>uncodepaged
-  else  char>uncodepaged 2drop  then
-  ;
 
 \ ==============================================================
 \ User interface
@@ -106,23 +103,22 @@ variable 'table  \ address of the translation table just defined
 public
 
 : uncodepage:  ( "name" -- )
+  new_table  dup 'table !  constant  depth depth0 !  ;
   \ Start the definition of a translation table.
   \ "name" = name of the translation table; it will return its address
-  new_table  dup 'table !  constant  depth depth0 !
-  ;
+
 : ;uncodepage  ( c'1 ca'1 len'1 ... c'n ca'n len'n -- )
+  #translations translations, ;
   \ End the definition of a translation table.
-  #translations translations,
-  ;
+
 : uncodepaged  ( ca len a  -- ca' len' )
+  uncodepaged-str str-clear
+  rot rot bounds ?do  dup i c@ >uncodepaged loop  drop
+  uncodepaged-str str-get ;
   \ Translate a string with a translation table.
   \ ca len = string to translate
   \ a = address of the translation table
   \ ca' len' = translated string
-  uncodepaged-str str-clear
-  rot rot bounds ?do  dup i c@ >uncodepaged loop  drop
-  uncodepaged-str str-get
-  ;
 
 end-package
 
@@ -151,10 +147,12 @@ table0 uncodepaged  type cr
 
 \ 2013-12-12 Written, based on <galope/translated.fs>, as a better
 \ alternative to ftrac
-\ (<http://programandala.net/es.program.ftrac.html>).
+\ (<http://programandala.net/en.program.ftrac.html>).
 \
 \ 2014-11-17: Module name updated.
 \
 \ 2017-08-17: Update change log layout. Update header.
 \
 \ 2017-08-18: Use `package` instead of `module:`.
+\
+\ 2017-12-04: Update source style.
