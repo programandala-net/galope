64fe50d9dc53c3e10f9bcafc8e8e2d8d00df1ec4
programandala.net <>
2018-07-08 16:55:50 +0000

    Fix and rewrite `lodge-resize`.
    FossilOrigin-Name: 2b9ae588491d3d4a11fd8447c6bec39b1a43b16eab5f93f8703c94a85e057db9

--- lodge-resize.fs
+++ lodge-resize.fs
@@ -9,25 +9,19 @@
 
 require ./lodge.fs
 
-: (lodge-resize) ( u a -- +n )
-  lodge ! /lodge @  swap /lodge +! ;
-  \ u  = additional address units already allocated in the lodge
-  \ a  = new address of the lodge
-  \ +n = offset to the new free space (it's the same than the
-  \      previous length of the lodge)
-
-: lodge-resize ( u -- +n ior )
-  lodge @ swap resize >r (lodge-resize) r> ;
+: lodge-resize ( u -- ior )
+  lodge @ over resize dup >r if   2drop
+                             else lodge ! /lodge !
+                             then r> ;
 
   \ doc{
   \
-  \ lodge-resize ( u -- +n ior )
+  \ lodge-resize ( u -- ior )
   \
   \ Resize the `lodge` to _u_ address units.  If the operation
-  \ succeeds, _+n_ is the offset to the additional free space
-  \ and _ior_ is zero.  If the operation fails, the value of
-  \ _+n_ is unimportant and _ior_ in the corresponding I/O
-  \ result code.
+  \ succeeds, `lodge` and `/lodge` are updated and _ior_ is zero.  If
+  \ the operation fails, `lodge` and `/lodge` remain untouched and
+  \ _ior_ in the corresponding I/O result code.
   \
   \ See: `lodge-allocate`, `lodge-allot`, `lodge-here`.
   \
@@ -37,3 +31,6 @@ require ./lodge.fs
 \ Change log
 
 \ 2017-11-12: Extract from <lodge.fs>. Improve documentation.
+\
+\ 2018-06-23: Fix and rewrite `lodge-resize`. Remove its factor
+\ `(lodge-resize)`.
