40d76eb5f2cef2fc400c75345f31a9986eda5553
programandala.net <>
2018-07-08 16:56:44 +0000

    Improve documentation.
    FossilOrigin-Name: 3f6477f7968bff6d7078d0da2967ef05ded416a9352f044ddddd9ecac080526c

--- replaced.fs
+++ replaced.fs
@@ -4,7 +4,7 @@
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Author: Marcos Cruz (programandala.net), 2014, 2016, 2017.
+\ Author: Marcos Cruz (programandala.net), 2014, 2016, 2017, 2018.
 
 \ ==============================================================
 
@@ -19,8 +19,15 @@ public
 
 : replaced ( ca1 len1 ca2 len2 ca3 len3 -- ca1' len1' )
   2rot tmp-str str-set  tmp-str str-replace  tmp-str str-get  ;
+
+  \ doc{
+  \
+  \ replaced ( ca1 len1 ca2 len2 ca3 len3 -- ca1' len1' )
+  \
   \ Replace all ocurrences of _ca3 len3_ ("needle") in _ca1 len1_
   \ ("haystack") with _ca2 len2_.
+  \
+  \ }doc
 
 end-package
 
@@ -36,3 +43,5 @@ end-package
 \ 2016-07-11: Update source layout and file header.
 \
 \ 2017-08-18: Use `package` instead of `module:`.
+\
+\ 2018-05-07: Improve documentation.
