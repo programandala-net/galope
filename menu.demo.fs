f47488f1d5c43f8e643525a80b86e49ed5ac7bb4
programandala.net <>
2018-07-30 14:41:29 +0000

    Make menus centerable and their margins configurable.
    FossilOrigin-Name: 518f4206420a13906d34cff0556910e6cfc863219350714ca7b264fcbe71c75c

--- menu.demo.fs
+++ menu.demo.fs
@@ -5,7 +5,7 @@
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Last modified: 201807301610
+\ Last modified: 201807301640
 \ See change log at the end of the file.
 
 \ Author: Marcos Cruz (programandala.net), 2018.
@@ -17,11 +17,11 @@ require ./menu.fs
 allocate-menu my-menu
 
  4 my-menu ~menu-column !
-15 my-menu ~menu-row !
-20 my-menu ~menu-width !
- 9 my-menu ~menu-height !
+16 my-menu ~menu-row !
+24 my-menu ~menu-width !
+ 8 my-menu ~menu-height !
 
-:noname ( -- ca len ) s" My title" ;
+:noname ( -- ca len ) s" My menu" ;
 
 my-menu ~menu-title-maker !
 
@@ -37,9 +37,37 @@ my-menu ~menu-title-maker !
 
 my-menu ~menu-option-maker !
 
+: .menu-option ( option | -1 )
+  cr dup -1 = if    ." You escaped the menu"
+              else  ." Your chose option "
+                    my-menu ~menu-option-maker perform type
+              then ;
+
+: press-any-key ( -- )
+  cr ." Press any key to continue" key drop ;
+
 : menu.demo ( -- option | -1 )
+
+  page ." Ceasing menu at column " my-menu ~menu-column @ .
+                          ." row " my-menu ~menu-row @ . ." :"
   my-menu .menu
-  my-menu ceasing-menu
+  my-menu ceasing-menu .menu-option
+  press-any-key
+
+  page ." Same ceasing menu, but automatically centered:"
+  my-menu center-menu
+  my-menu ceasing-menu .menu-option
+  press-any-key
+
+  page ." Again, but with larger margins:"
+  2 to menu-top-margin
+  2 to menu-left-margin
+  2 to menu-right-margin
+  \ 3 to menu-bottom-margin
+  my-menu ceasing-menu .menu-option
+  press-any-key
+
+  page ." The End"
   \ my-menu free throw
   ;
 
