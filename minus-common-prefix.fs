5a91f7d47d9cd27203a5e4d0afd2565aaf67f10d
programandala.net <>
2018-09-27 15:08:46 +0000

    Add `-common-suffix`.
    FossilOrigin-Name: 1ac9302bfa2a841eca743bb35fd679d466385bb1a98176f1199e55cc7c5c4843

---
+++ minus-common-prefix.fs
@@ -0,0 +1,39 @@
+\ galope/minus-common-prefix.fs
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2018.
+
+\ ==============================================================
+
+require ./both-lengths.fs
+
+: -common-prefix ( ca1 len1 ca2 len2 -- ca3 len3 ca4 len4 )
+  0 0 {: prefix-length char1 :}
+  both-lengths min 0 ?do
+    2swap over i chars + c@ to char1 
+    2swap over i chars + c@    char1 <> if leave then
+    i 1+ to prefix-length
+  loop prefix-length /string 2swap
+       prefix-length /string 2swap ;
+
+  \ doc{
+  \
+  \ -common-prefix ( ca1 len1 ca2 len2 -- ca3 len3 ca4 len4 )
+  \
+  \ Remove the prefix which is common to strings _ca1 len1_ and _ca2
+  \ len2_, returning their corresponding endings _ca3 len3_ and _ca4
+  \ len4_.
+  \
+  \ This word does not work with multi-byte characters.
+  \
+  \ See: `-prefix`.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
+\ 2018-09-27: Start.
+
