42de89549f4b23aebd394238dff616bf8febee93
Marcos Cruz <mercurial-repo@programandala.net>
2019-03-11 20:51:10 +0000

    Add `noname-create`. Needed by the multilingual addon of Fendo.
    FossilOrigin-Name: 66294d113d9e4337a31038d0a278de1b74213f42a92f63d907964db2f781ec78

---
+++ noname-create.fs
@@ -0,0 +1,25 @@
+\ galope/noname-create.fs
+
+\ This file is part of Galope
+\ http://programandala.net/en.program.galope.html
+
+\ Author: Marcos Cruz (programandala.net), 2019.
+
+\ ==============================================================
+
+: noname-create  ( -- xt ) noname create latestxt ;
+
+  \ doc{
+  \
+  \ noname-create ( -- xt )
+  \
+  \ Create an unnamed ``create`` and return its _xt_.
+  \
+  \ See: `noname-variable`, `noname-constant`, `noname-field`.
+  \
+  \ }doc
+
+\ ==============================================================
+\ Change log
+
+\ 2019-03-11: Start.
--- noname-two-constant.fs
+++ noname-two-constant.fs
@@ -1,9 +1,9 @@
-\ galope/colon-noname-two-constant.fs
+\ galope/noname-two-constant.fs
 
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ Author: Marcos Cruz (programandala.net), 2013, 2017.
+\ Author: Marcos Cruz (programandala.net), 2013, 2017, 2019.
 
 \ ==============================================================
 
@@ -16,7 +16,7 @@
   \ Create an unnamed double-cell constant with value _x1 x2_ and
   \ return its _xt_.
   \
-  \ See: `noname-constant`, `noname-2variable`.
+  \ See: `noname-constant`, `noname-2variable`, `noname-create`.
   \
   \ }doc
 
@@ -28,3 +28,5 @@
 \ 2017-08-17: Update change log layout and source style.
 \
 \ 2017-08-24: Remove the initial colon from the name. Document.
+\
+\ 2019-03-11: Fix title. Update documentation.
