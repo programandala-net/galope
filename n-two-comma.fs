579b24e61d2b909d7e779f1cb87f5bfd501f7171
programandala.net <>
2019-12-27 17:54:39 +0000

    Add `n,`.
    FossilOrigin-Name: 686c788a4a14ff06f5324e8a8ffea0cf5c0551bf1f65e5c6c3fdc237c7c46ee5

--- n-two-comma.fs
+++ n-two-comma.fs
@@ -3,7 +3,7 @@
 \ This file is part of Galope
 \ http://programandala.net/en.program.galope.html
 
-\ By Marcos Cruz (programandala.net), 2017.
+\ By Marcos Cruz (programandala.net), 2017, 2019.
 
 \ ==============================================================
 
@@ -15,9 +15,13 @@
   \
   \ Execute ``2,`` _n_ times.
   \
+  \ See: `n,`.
+  \
   \ }doc
 
 \ ==============================================================
 \ Change log
 
 \ 2017-12-04: Start. Extract from <sarray.fs> and rename.
+\
+\ 2019-12-27: Update documentation.
