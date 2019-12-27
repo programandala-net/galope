47fe15a9422c1279a7dbbdab5b8171d0044eae9e
Marcos Cruz <mercurial-repo@programandala.net>
2019-12-27 21:47:33 +0000

    Document `th`.
    FossilOrigin-Name: b55ee5b574a54c98028a13e74852c0599709157920f83c394591c1b352b9caee

--- th.fs
+++ th.fs
@@ -7,9 +7,19 @@
 
 : th ( a1 u -- a2 ) cells + ;
 
+  \ doc{
+  \
+  \ th ( a1 u -- a2 )
+  \
+  \ Return address _a2_ of cell item _u_ in table _a1_.
+  \
+  \ }doc
+
 \ ==============================================================
 \ Change log
 
 \ 2012-05-06 Added.
 \
 \ 2017-08-17: Update change log layout. Update header.
+\
+\ 2019-12-27: Document.
